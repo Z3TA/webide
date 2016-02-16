51d2f0ad6f3fd0a3c67ded36b33712ecb2b2358f
Z <zeta@zetafiles.org>
2016-02-16 17:00:39 +0100

    Started with a debugger!

---
+++ plugin/chrome_debugger.js
@@ -0,0 +1,18 @@
+(function() {
+	/*
+		This file uses the Chrome debugger:
+		https://developer.chrome.com/devtools/docs/protocol/1.1/index
+		
+		1) Start the app with --remote-debugging-port=9222
+		
+		2) Connect to http://localhost:9222/json
+		
+		3) Connect to "webSocketDebuggerUrl"
+		
+		4) Prosper!
+		
+		
+		
+	*/
+		})();
+		
