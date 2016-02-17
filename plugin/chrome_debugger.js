f69b986a275a4d2cb4fe9534d7eece2f1b5782a7
Z <zeta@zetafiles.org>
2016-02-17 14:39:33 +0100

    InStructions for making a self debugger. But use the built nw.js debugger for now.

--- plugin/chrome_debugger.js
+++ plugin/chrome_debugger.js
@@ -11,8 +11,24 @@
 		
 		4) Prosper!
 		
+		PS. Reloading the app will disable the debugger! And we'll have to reconnect.
+		
+		Self debugging
+		--------------
+		* Check if the app is running with the --remote-debugging-port option.
+		* Connect to the debugger
+		* Open a console file to display console messages
+		* Enable Setbreakpoint in editor menu
+		* When a breakpoint is fired, collect all variables, alert, then show the file and scroll to the breakpoint.
+		* Remove breakpoint.
+		* When mouse-over a variable, show the variable collected.
+		
+		* If an error is thrown, show an dialog: "Jump to error", if yes:
+		* open the file, scroll down to the line and show the error message.
+		
 		
 		
 	*/
+	
 		})();
 		
