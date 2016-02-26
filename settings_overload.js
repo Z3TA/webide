cda43bd1602664592f36bf3676cdc5d53b03b43a
Z <zeta@zetafiles.org>
2016-02-26 11:07:53 +0100

    Possible to overload settings

---
+++ settings_overload.js
@@ -0,0 +1,9 @@
+/*
+	This file will overload the settings.js
+	
+	(You might want to add it to .hgignore .gitignore or equivalent)
+	
+*/
+
+//global.settings.style.font = "Lucida Console";
+//global.settings.sub_pixel_antialias = false;
