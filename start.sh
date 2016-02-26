5ba2ae700822ed087872d4626570d4020bec3a19
Z <zeta@zetafiles.org>
2016-02-26 07:41:24 +0100

    Linux runtime added

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update the path to the icon
+mv JZedit.desktop JZedit.desktop-bak
+sed -e "s,Icon=.*,Icon=$PWD/gfx/bean3.png,g" JZedit.desktop-bak > JZedit.desktop
+rm JZedit.desktop-bak
+
+# Make it runable again
+chmod +x JZedit.desktop
+
+# Start the editor
+runtime/nwjs-v0.12.3-linux-x64/nw .
