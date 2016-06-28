065142badd2f93937a15f13ed3da37641fd5282a
Z <zeta@zetafiles.org>
2016-06-28 15:10:26 +0200

    When I close a file that is not saved (Ctrl-Q) and get a confirm box, the functions returns void? instead of false

--- plugin/file_close.js
+++ plugin/file_close.js
@@ -26,9 +26,16 @@
 			var close = true;
 			
 			// Check if it's saved first!
-			if(!file.isSaved) close = confirm("All unsaved changes will be lost, are you sure you want to close this file? " + file.path);
+			if(!file.isSaved) {
+				// Language?
+				var yes = "Yes, close it";
+				var no = "No, keep it open";
+				
+				confirmBox("All unsaved changes will be lost, are you sure you want to close this file?<br><b>" + file.path + "</b>", [yes, no], function(answer) {
+					if(answer == yes) editor.closeFile(editor.currentFile.path);
+				});
+			}
 			
-			if(close) editor.closeFile(editor.currentFile.path);
 		}
 		else {
 			// Close the editor!?
