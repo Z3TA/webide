2db2f5c2e60bc1b94ad6f9424860e5dc910611b0
Z <zeta@zetafiles.org>
2016-06-28 15:14:58 +0200

    Can be used for testing dialog boxes. Could probably be removed.

---
+++ plugin/test_dialog.js
@@ -0,0 +1,23 @@
+editor.on("start", testDialogs);
+
+function testDialogs() {
+	
+	//alertBox("This is the final warning", "warning");
+	
+	//alertBox("Anim cupidatat consectetur non ut id est irure excepteur laboris pariatur magna enim ut duis aute sint cillum.<br>Tempor reprehenderit ex ea mollit Lorem duis ut laboris occaecat voluptate et irure tempor incididunt in ipsum exercitation consequat ipsum cillum eiusmod dolore labore qui.", "error");
+	
+	//alertBox("<h1>OMG</h1><p>This is crazy</p>");
+	
+	//alert("dude");
+	
+	//confirmBox("Are you sure you want eat this?", ["Yes!", "Erm, why are you asking?", "Another anwser1", "Another anwser2", "Another anwser3", "Another anwser4"], function(answer) {alert(answer);});
+	
+	//confirm("Are you sure you want eat this?");
+	
+	//promptBox("What is your name?", false, function(answer) {alert(answer);});
+	
+	//var q = prompt("What is your name?");
+	
+	//alert(q);
+	
+}
