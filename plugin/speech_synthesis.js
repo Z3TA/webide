a6ee60a3ed39182d8cb4019b32a7182b50e3ddb3
Z <zeta@zetafiles.org>
2016-08-23 14:20:43 +0200

    Tested the speech synthesis. But will have to upgrade NW.JS!

---
+++ plugin/speech_synthesis.js
@@ -0,0 +1,29 @@
+(function() {
+	"use strict";
+	
+	editor.plugin({
+		desc: "Will speak whats on the line",
+		load: load,
+		unload: unload,
+	});
+	
+	function load() {
+		// Called when the module is loaded
+		if('speechSynthesis' in window){
+			var speech = new SpeechSynthesisUtterance('hello baby');
+			speech.lang = 'en-US';
+			window.speechSynthesis.speak(speech);
+			alert("You should here a sound!");
+		}
+		else alert("speechSynthesis not available!");
+		
+	}
+	
+	function unload() {
+		
+	}
+	
+	
+})();
+
+
