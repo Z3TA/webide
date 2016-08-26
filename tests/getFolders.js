b9d36404a8a8d5ed5073fb27124de7db00b3ec3f
Z <zeta@zetafiles.org>
2016-08-26 11:21:56 +0200

    file doesn't need to be encapsulated by a self calling function because it only consist of a function call

--- tests/getFolders.js
+++ tests/getFolders.js
@@ -1,30 +1,25 @@
-(function() {
-	"use strict";
+editor.addTest(function getFoldersTest(callback) {
+	// Testing the global getFolders function 
 	
-	editor.addTest(function getFoldersTest(callback) {
-		// Testing the global getFolders function 
-		
-		var ftpSite = getFolders("ftp://user:pw@hostname/folder1/folder2/folder3");
-		
-		if(!(ftpSite[0] == "/folder1" &&
-		ftpSite[1] == "/folder1/folder2" &&
-		ftpSite[2] == "/folder1/folder2/folder3")) throw new Error("ftpSite=" + JSON.stringify(ftpSite));
-		
-		
-		var windowsPath = getFolders("C:\\Windows\\System32");
-		
-		if(!(windowsPath[0] == "C:\\Windows" &&
-		windowsPath[1] == "C:\\Windows\\System32")) throw new Error("windowsPath=" + JSON.stringify(windowsPath));
-		
-		
-		var unixPath = getFolders("/var/log/");
-		
-		if(!(unixPath[0] == "/var" &&
-		unixPath[1] == "/var/log")) throw new Error("unixPath=" + JSON.stringify(unixPath));
-		
-		callback(true);
-			
-		
-	}, 1);
+	var ftpSite = getFolders("ftp://user:pw@hostname/folder1/folder2/folder3");
 	
-})();
+	if(!(ftpSite[0] == "/folder1" &&
+	ftpSite[1] == "/folder1/folder2" &&
+	ftpSite[2] == "/folder1/folder2/folder3")) throw new Error("ftpSite=" + JSON.stringify(ftpSite));
+	
+	
+	var windowsPath = getFolders("C:\\Windows\\System32");
+	
+	if(!(windowsPath[0] == "C:\\Windows" &&
+	windowsPath[1] == "C:\\Windows\\System32")) throw new Error("windowsPath=" + JSON.stringify(windowsPath));
+	
+	
+	var unixPath = getFolders("/var/log/");
+	
+	if(!(unixPath[0] == "/var" &&
+	unixPath[1] == "/var/log")) throw new Error("unixPath=" + JSON.stringify(unixPath));
+	
+	callback(true);
+
+});
+	
