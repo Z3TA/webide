11ac5d305eab08a80729123cbbf4201354281b11
Z <zeta@zetafiles.org>
2016-10-04 07:54:56 +0200

    Finished printing plugin. It's a hack, but will do for now

--- plugin/print_text.js
+++ plugin/print_text.js
@@ -1,4 +1,8 @@
 (function() {
+	/*
+		Naive priting function
+	*/
+	
 	"use strict";
 
 var gui = require('nw.gui');
@@ -22,28 +26,28 @@ var gui = require('nw.gui');
 		editor.hideMenu();
 		
 		//var windowContent = editor.currentFile.text;
-		
-		style = "white-space: pre-wrap;"
-		
-		//var windowContent = "<style>"white-space: pre-wrap;"" +  + "<<title>" + editor.currentFile.path + "</title><pre>" + editor.currentFile.text + "</pre>";
-		
 		//var url = "file:///" + editor.currentFile.path;
+		
+		// New document needs to be a HTML file (unless the url is opened) ...
+		var windowContent = '<title>' + editor.currentFile.path + '</title><pre style="white-space: pre-wrap;">' + editor.currentFile.text + '</pre>';
 		
 		//var printWin = gui.Window.open(editor.currentFile.path);
 		
 		var printWin = window.open('text/plain');
 		printWin.document.write(windowContent);
-		printWin.document.close();
+		printWin.document.close(); // Need this or no print dialog will show
 		printWin.focus();
 		printWin.print();
-		//printWin.close();
+		
+		setTimeout(function() {
+			// Can't close right away or it will also close the editor, no idea why
+		printWin.close();
+		}, 500); // Need to be at least 500ms
+		
 		
 		//printWin.window.print();
-		
 		//printWin.window.close();
-		
 		//printWin.on("loaded", function() {});
-		
 		
 	}
 	
