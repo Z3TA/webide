95877ff3767b74309f92f1ca9ce7006fbd2d7ba8
Z <zeta@zetafiles.org>
2016-11-14 16:10:26 +0100

    no temp/release folder in archive files

--- release.sh
+++ release.sh
@@ -9,24 +9,24 @@ name=jzedit
 echo $name-v$version$beta-$commit
 
 
-# Delete old files if they exists
+echo "Delete old files if they exists"
 rm -rf temp/release/linux/
 rm -rf temp/release/windows/
 rm -rf temp/release/osx/
 
-# Create the temporary directory if it doesn't exist
+echo "Create the temporary directory if it doesn't exist"
 mkdir -p temp/release/linux
 mkdir -p temp/release/windows
 mkdir -p temp/release/osx
 
-# Copy the files
+echo "Copy the files"
 hg clone . temp/release/linux/
 
-# Set devMode and toolbar to false
+echo "Set devMode and toolbar to false"
 sed -i -e 's/devMode: true/devMode: false/g' temp/release/linux/editor.js
 sed -i -e 's/"toolbar": true/"toolbar": false/g' temp/release/linux/package.json
 
-# Clean up
+echo "Clean up"
 rm -rf temp/release/linux/.hg/
 rm -rf temp/release/linux/release.sh
 rm -rf temp/release/linux/todo.md
@@ -39,18 +39,18 @@ rm -rf temp/release/linux/.hgignore
 # find temp/release/linux/ -name '*.js' | xargs uglifyjs
 
 
-# Copy over version.inc
+echo "Copy over version.inc"
 cp version.inc temp/release/linux/
 
 
-# Make a Windows release
+echo "Make a Windows release"
 cp -rf temp/release/linux/. temp/release/windows/
 
-# Make a OSX release
+echo "Make a OSX release"
 cp -rf temp/release/linux/. temp/release/osx/
 
 
-# Clean up the Linux release
+echo "Clean up the Linux release"
 rm -rf temp/release/linux/runtime/nwjs-v0.12.3-win-x64/
 rm -rf temp/release/linux/runtime/nwjs-v0.12.3-osx-x64/
 rm -rf temp/release/linux/plugin/spellcheck/nodehun_windows.node
@@ -58,7 +58,7 @@ rm -rf temp/release/linux/start.bat
 rm -rf temp/release/linux/create_shortcut.vbs
 rm -rf temp/release/linux/osx_start.sh
 
-# Clean up the Windows release
+echo "Clean up the Windows release"
 rm -rf temp/release/windows/runtime/nwjs-v0.12.3-linux-x64
 rm -rf temp/release/windows/runtime/nwjs-v0.12.3-osx-x64/
 rm -rf temp/release/windows/plugin/spellcheck/nodehun_linux.node
@@ -66,7 +66,7 @@ rm -rf temp/release/windows/start.sh
 rm -rf temp/release/windows/JZedit.desktop
 rm -rf temp/release/windows/osx_start.sh
 
-# Clean up the OSX release
+echo "Clean up the OSX release"
 rm -rf temp/release/osx/runtime/nwjs-v0.12.3-win-x64
 rm -rf temp/release/osx/runtime/nwjs-v0.12.3-linux-x64/
 rm -rf temp/release/osx/plugin/spellcheck/nodehun_linux.node
@@ -76,20 +76,33 @@ rm -rf temp/release/osx/JZedit.desktop
 rm -rf temp/release/osx/start.bat
 rm -rf temp/release/osx/create_shortcut.vbs
 
-# zip and remove the Windows release (can't be run under Windows git bash)
-zip -9 -y -r -q temp/release/$name-v$version$beta-$commit-win-x64.zip temp/release/windows
-rm -rf temp/release/windows/
+echo "zip and remove the Windows release (cant be run under Windows git bash)"
+cd temp/release/
+mv windows $name-v$version$beta-$commit
+zip -9 -y -r -q $name-v$version$beta-$commit-win-x64.zip $name-v$version$beta-$commit
+cd ..
+cd ..
+rm -rf temp/release/$name-v$version$beta-$commit
 
 
-# Create a tarball and compress it for the Linux release
-tar -zcf temp/release/$name-v$version$beta-$commit-linux-x64.tar.gz temp/release/linux
-rm -rf temp/release/linux/
+echo "Create a tarball and compress it for the Linux release"
+cd temp/release/
+mv linux $name-v$version$beta-$commit
+tar -zcf $name-v$version$beta-$commit-linux-x64.tar.gz $name-v$version$beta-$commit
+cd ..
+cd ..
+rm -rf temp/release/$name-v$version$beta-$commit
 
-# Create a tarball and compress it for OSX
-tar -zcf temp/release/$name-v$version$beta-$commit-osx-x64.tar.gz temp/release/osx
-rm -rf temp/release/osx/
+echo "Create a tarball and compress it for OSX"
+cd temp/release/
+mv osx $name-v$version$beta-$commit
+tar -zcf $name-v$version$beta-$commit-osx-x64.tar.gz $name-v$version$beta-$commit
+cd ..
+cd ..
+rm -rf temp/release/$name-v$version$beta-$commit
 
-# Remove files no longer needed
+
+echo "Remove files no longer needed"
 rm version.inc
 
 
@@ -99,4 +112,4 @@ rm version.inc
 
 # Update the RSS.xml
 
-
+echo "Done!"
