d68717b78027db3b42a8a0b79b99ea4936b15496
Z <zeta@zetafiles.org>
2017-03-07 22:38:08 +0100

    Trying to find ways to run a chromeless browser window ...

---
+++ Run in Chrome.bat
@@ -0,0 +1,5 @@
+REM Windows 7: \Users\[you]\AppData\Local\Google\Chrome\Application\chrome.exe
+REM Windows 8: C:\Program Files\Google\Chrome\Application 
+REM "%programfiles(x86)%\Google\Chrome\Application" --app="index.htm"
+REM Not sure if this will work: (haven't tested)
+"%userprofile%\AppData\Local\Google\Chrome\Application\chrome.exe" --app="index.htm"
---
+++ Run in Firefox.bat
@@ -0,0 +1,1 @@
+"%programfiles(x86)%\Mozilla Firefox\firefox.exe" -chrome "index.htm"
