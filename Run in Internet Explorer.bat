470764f1f2367b08294e18349becf231d34ce072
Z <zeta@zetafiles.org>
2017-03-08 16:55:47 +0100

    more info about IE kiosk mode

--- Run in Internet Explorer.bat
+++ Run in Internet Explorer.bat
@@ -1,4 +1,5 @@
 REM Internet Explorer in kiosk mode
+REM more info: https://support.microsoft.com/en-us/help/154780/how-to-use-kiosk-mode-in-microsoft-internet-explorer
 SET URL="file:///%~dp0index.htm"
 echo %URL%
 start iexplore -k %URL%
