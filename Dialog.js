23069c4344342a8bc3c044514440cac0e23f6794
Z <zeta@zetafiles.org>
2017-03-09 10:34:02 +0100

    Dialogs into it's own file

---
+++ Dialog.js
@@ -0,0 +1,215 @@
+/*
+	
+	Dialog functions
+
+	
+	
+*/
+
+"use strict";
+
+function Dialog(msg, icon) {
+	
+	if(msg == undefined) throw new Error("Dialog without a message!");
+	
+	msg = msg.toString(); // Convert numbers etc to string so we can use the replace method
+	
+	//console.log("Dialog msg=" + msg);
+	
+	msg = msg.replace(/\n/g, "<br>");
+	
+	var body = document.getElementById("body");
+	
+	if(!body) {
+		console.warn("Dialog created before html body is available");
+		return;
+	}
+	
+	var message = document.createElement("div");
+	message.setAttribute("class", "message");
+	message.innerHTML = msg; // Support HTML
+	
+	var img;
+	
+	if(icon == "warning" ) {
+		img = document.createElement("img");
+		img.setAttribute("src", "gfx/warning.svg");
+	}
+	else if(icon == "error") {
+		img = document.createElement("img");
+		img.setAttribute("src", "gfx/error.svg");
+	}
+	else if(icon) {
+		throw new Error("Dialog icon not supported: " + icon);
+	}
+	
+	
+	this.div = document.createElement("div");
+	
+	var div = this.div;
+	
+	div.setAttribute("class", "dialog");
+	div.setAttribute("style", "position: absolute; top: 50px; left: 50px");
+	
+	div.addEventListener("click", focusDefault, false);
+	
+	div.appendChild(message);
+	
+	body.appendChild(div);
+	
+	if(img) {
+		//alert(icon);
+		// If an icon is used adjust it's size
+		var messageHeight = parseInt(message.offsetHeight);
+		console.log("messageHeight=" + messageHeight);
+		img.setAttribute("height", Math.round(messageHeight / 2));
+		
+		if(message.childNodes.length == 1) {
+			img.style.verticalAlign = "middle";
+			img.style.float = "none";
+			img.style.display = "inline-block";
+		}
+		else {
+			img.style.float = "left";
+		}
+		
+		message.insertBefore(img, message.firstChild);
+	}
+	
+	// Get the computed size of the box
+	var divHeight = parseInt(div.offsetHeight);
+	var divWidth = parseInt(div.offsetWidth);
+	
+	// Place it in the middle of the screen
+	var windowHeight = parseInt(window.innerHeight);
+	var windowWidth = parseInt(window.innerWidth);
+	
+	var sligtlyUp = 32; // Space for buttons and stuff
+	
+	div.style.top = Math.round(windowHeight / 2 - divHeight/2 - sligtlyUp) + "px";
+	div.style.left = Math.round(windowWidth / 2 - divWidth/2) + "px";
+	
+	
+	// Give the focus to the box
+	editor.input = false;
+	
+	// Give the program time to add buttons etc to the dialog
+	// Also avoid accidently closing the dialog (while typing spaces)
+	var dialogDelay = 2000;
+	setTimeout(focusDefault, dialogDelay); 
+	
+	function focusDefault() {
+		// Give focus to the element with attribute focus:true
+		
+		var childElement = div.childNodes;
+		for (var i=0; i<childElement.length; i++) {
+			if(childElement[i].getAttribute("focus") == "true") {
+				childElement[i].focus();
+				break;
+			}
+		}
+		
+		editor.input = false;
+	}
+}
+Dialog.prototype.close = function() {
+	this.div.parentElement.removeChild(this.div);
+	
+	// The editor watches for clicks outside the "editor" area (canvas), so wait until that is done before giving back input
+	setTimeout(function() {
+		editor.input = true;
+	}, 500); // Not too fast, make sure the user has released the space bar
+}
+
+function alertBox(msg, icon) {
+	var dialog = new Dialog(msg, icon);
+	
+	var button = document.createElement("button");
+	button.setAttribute("class", "alert");
+	button.setAttribute("focus", "true");
+	button.appendChild(document.createTextNode("OK"));
+	
+	button.addEventListener("click", function() {dialog.close()}, false);
+	
+	dialog.div.appendChild(button);
+	
+	return dialog;
+}
+
+
+//window.alert = alertBox; // Override the native alert box
+
+/*
+	Example reason why you want to use custom confirm box:
+	* Native confirm box registers a keyPress if it was called on a keydown event
+*/
+function confirmBox(msg, options, callback) {
+	
+	var dialog = new Dialog(msg);
+	
+	for (var i=0; i<options.length; i++) {
+		makeButton(i);
+	}
+	
+	return dialog;
+	
+	function makeButton(i) {
+		var txt = options[i];
+		var button = document.createElement("button");
+		button.setAttribute("class", "confirm");
+		
+		// The last button will be the default (get focus)
+		if(i == (options.length -1)) button.setAttribute("focus", "true");
+		
+		button.appendChild(document.createTextNode(txt));
+		
+		button.addEventListener("click", function() {callback(txt); dialog.close();}, false);
+		
+		dialog.div.appendChild(button); 
+	}
+	
+}
+
+function promptBox(msg, isPassword, callback) {
+	var dialog = new Dialog(msg);
+	
+	var input = document.createElement("input");
+	
+	if(isPassword) input.setAttribute("type", "password")
+	else input.setAttribute("type", "text");
+	
+	input.setAttribute("class", "input prompt");
+	input.setAttribute("focus", "true");
+	
+	var ok = document.createElement("button");
+	ok.setAttribute("class", "prompt");
+	ok.setAttribute("type", "submit");
+	ok.appendChild(document.createTextNode("OK"));
+	
+	ok.addEventListener("click", function() {callback(input.value); dialog.close()}, false);
+	
+	
+	var cancel = document.createElement("button");
+	cancel.setAttribute("class", "prompt");
+	cancel.appendChild(document.createTextNode("Cancel")); // Language?
+	
+	cancel.addEventListener("click", function() {callback(null); dialog.close()}, false);
+	
+	
+	input.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		var enterKey = 13;
+		var escapeKey = 27;
+		// Clicking enter in the input area should "submit"
+		if (event.keyCode == enterKey) ok.click()
+		// Clicking escape should be same as cancel
+		else if(event.keyCode == escapeKey) cancel.click();
+	});
+	
+	
+	dialog.div.appendChild(input);
+	dialog.div.appendChild(cancel);
+	dialog.div.appendChild(ok);
+	
+	return dialog;
+}
