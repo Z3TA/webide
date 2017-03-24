0d097ccffdbeb6f46790f84f7a5c5e39b531a2a7
Z <zeta@zetafiles.org>
2017-03-24 16:03:12 +0100

    Maybe use this for native users on OSX

---
+++ start.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/*
+	Starts the editor ...
+	
+	In Apple producs, we need to have a developer licence, 
+	or users will not be able to run our scripts or apps ...
+	Users can run Node-JS scripts if NodeJS is installed though!
+	
+*/
+
+	
+	// Check if server is running on localhost
+	
+	// Check if server is running on private subnet
+	
+	// Start a server if we did not find any
+	
+// Attempt to start the client using nw.js
+
+	// Check for Chrome/Chromium
+	
+	// Check for Firefox
+	
+	// Check for IE/Edge
+	
+	// Check for Safari
+	
+// Launch the client in any of the browsers detected
+	
+	
+	
+	
