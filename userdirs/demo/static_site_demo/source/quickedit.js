3c301986d87b2b908a8a6a955bfff617e656cd1d
Z <zeta@zetafiles.org>
2017-04-07 15:11:28 +0200

    cleanup

--- userdirs/demo/static_site_demo/source/quickedit.js
+++ userdirs/demo/static_site_demo/source/quickedit.js
@@ -3,7 +3,7 @@
 	Inlcuding this file on your web page makes it possible to tell
 	jzedit to edit the page you are looking at ...
 	
-	The "secret" combo is left mouse click while holding Ctrl and Alt key
+	The "secret" combo is left Mouse click while holding Ctrl and Alt key
 	It will take you to the editor, where you can change the source code.
 	
 */
@@ -12,15 +12,11 @@
 	
 	"use strict";
 	
-	alert("Hello from quickedit.js");
-	
 	function editPage(nodes, url) {
 		var editorUrl = "http://127.0.0.1/"; // Change this to the editor URL
 		
 		document.location = editorUrl + "?editPage=" + encodeURIComponent(url) + "&nodes=" + encodeURIComponent(nodes.join(",")) + "";
-
 	}
-	
 	
 	document.addEventListener( "click", function(e) {
 		console.log(event);
@@ -55,38 +51,29 @@
 					el = e.path[i];
 					
 					// innerHTML, localName, outerHTML, tagName, nodeName
-					clickPath.push({
-						innerHTML: e.path[i].innerHTML,
-						outerHTML: e.path[i].outerHTML,
-						localName: e.path[i].localName,
-						tagName: e.path[i].tagName,
-						nodeName: e.path[i].nodeName,
-					});
 					
 					nodes.push(e.path[i].localName);
 					
 				}
 			}
 			
-			//nodes.push(word);
-			
 			var url = window.location.href;
 			
-			console.log("url=" + url);
-			console.log("clickPath=" + JSON.stringify(clickPath, null, 2));
-			console.log("nodes=" + JSON.stringify(nodes, null, 2));
+			/*
+				console.log("url=" + url);
+				console.log("clickPath=" + JSON.stringify(clickPath, null, 2));
+				console.log("nodes=" + JSON.stringify(nodes, null, 2));
+			*/
 			
 			editPage(nodes, url);
 		}
 		else {
-			console.log("e.which=" + e.which);
-			console.log("e.ctrlKey=" + e.ctrlKey);
-			console.log("e.altKey=" + e.altKey);
-			console.log("e.shiftKey=" + e.shiftKey);
+			/*
+				console.log("e.which=" + e.which);
+				console.log("e.ctrlKey=" + e.ctrlKey);
+				console.log("e.altKey=" + e.altKey);
+				console.log("e.shiftKey=" + e.shiftKey);
+			*/
 		}
 	});
-	
-
-	
-	
 })();
