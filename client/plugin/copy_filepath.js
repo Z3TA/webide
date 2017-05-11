89bec9c6fe9fdbad357ba47da36d3538a16a294c
Z <zeta@zetafiles.org>
2017-05-11 11:30:53 +0200

    Made it into a plugin and made it work in the browser

--- client/plugin/copy_filepath.js
+++ client/plugin/copy_filepath.js
@@ -1,40 +1,55 @@
 (function() {
 	"use strict";
 	
-	EDITOR.on("start", function editorStarted() {
-		
-		//EDITOR.bindKey({desc: "Show the manager for the static site generator", fun: show, charCode: keyF9, combo: CTRL});
-		
-		EDITOR.addMenuItem("Copy file path", function copyFilePath() {
+	var menuItem;
+	
+	EDITOR.plugin({
+		desc: 'Adds "Copy file path" to the context menu',
+		load: function load() {
 			
-			// Puts the current file path into the clipboard
+			menuItem = EDITOR.addMenuItem("Copy file path", function copyFilePath() {
+				
+				// Puts the current file path into the clipboard
+				
+				if(EDITOR.currentFile) {
+					
+					var text = EDITOR.currentFile.path;
+					
+					if(runtime == "nw.js") {
+					
+					// Load native UI library
+					var gui = require('nw.gui');
+					
+					// We can not create a clipboard, we have to receive the system clipboard
+					var clipboard = gui.Clipboard.get();
+					
+					// Read from clipboard
+					//var text = clipboard.get('text');
+					//console.log(text);
+					
+					// Or write something
+						clipboard.set(text, 'text');
+					
+					// And clear it!
+					//clipboard.clear();
+					}
+					else {
+						 window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+					}
+				}
+				else {
+					alert("No file open!");
+				}
+				
+				EDITOR.hideMenu();
+				
+				
+			});
 			
-			if(EDITOR.currentFile) {
-				// Load native UI library
-				var gui = require('nw.gui');
-				
-				// We can not create a clipboard, we have to receive the system clipboard
-				var clipboard = gui.Clipboard.get();
-				
-				// Read from clipboard
-				//var text = clipboard.get('text');
-				//console.log(text);
-				
-				// Or write something
-				clipboard.set(EDITOR.currentFile.path, 'text');
-				
-				// And clear it!
-				//clipboard.clear();
+		},
+		unload: function unload() {
+			EDITOR.removeMenuItem(menuItem);
 			}
-			else {
-				alert("No file open!");
-			}
-			
-			EDITOR.hideMenu();
-			
-			
 		});
-		
-	});
 	
 })();
