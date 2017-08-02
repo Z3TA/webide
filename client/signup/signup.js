173aa5c3059658f5a877417f4b8c0c189f0d1c8b
Z <zeta@zetafiles.org>
2017-08-02 16:17:51 +0200

    more work on the signup service

---
+++ client/signup/signup.js
@@ -0,0 +1,150 @@
+var SOCKJS_OPEN = 1;
+var EDITOR_URL = "http://webide.se/";
+var SIGNUP_URL = "http://127.0.0.1:8100/signup";
+var CHARCODE_ENTER = 13;
+var MIN_PW_LENGTH = 5;
+var MIN_USERNAME_LENGTH = 3;
+
+window.onload = function main() {
+	var inputUsername = document.getElementById("username");
+	var inputPassword = document.getElementById("password");
+	var createButton = document.getElementById("createButton");
+	var usernameAlertDiv = document.getElementById("usernameAlert");
+	var pwAlertDiv = document.getElementById("pwAlert");
+	var generalAlertDiv = document.getElementById("generalAlert");
+	var typingTimer;
+	
+	generalAlertDiv.style.display = "none";
+	usernameAlertDiv.style.display = "none";
+	pwAlertDiv.style.display = "none";
+	
+	inputUsername.addEventListener("keyup", inputUserNameKeyUp);
+	inputPassword.addEventListener("keyup", inputPasswordKeyUp);
+	createButton.addEventListener("click", createButtonClick);
+	
+	var sockJsReservedQuirk = '';
+	var sockJsOptions = {debug: true};
+	connection = new SockJS(SIGNUP_URL, sockJsReservedQuirk, sockJsOptions);
+	connection.onopen = function serverConnected() {
+		console.log("Connected to signup service!");
+	};
+	
+	connection.onmessage = function serverMessage(e) {
+		var msg = e.data;
+		
+		console.log("Message from signup service: " + msg);
+		
+		var arr = msg.split(":");
+		var code = arr[0];
+		
+		generalAlertDiv.style.display = "none";
+		usernameAlertDiv.style.display = "none";
+		pwAlertDiv.style.display = "none";
+		
+		if(code == "availableError") alertUsername("Problem checking if username " + arr[1] + " is available: " + arr[2]);
+		else if(code == "available") {
+			if(arr[2]=="false") alertUsername("Username " + arr[1] + " is " + (arr[2]=="true" ? "" : "not") + " available!");
+			}
+		else if(code == "createError") alertGeneralMessage("Unable to create username " + arr[1] + ": " + arr[2]);
+		else if(code == "serviceError") alertGeneralMessage(arr[1]);
+		else if(code == "created") {
+			alertGeneralMessage("Successfully created username" + arr[1]);
+			document.location="http://" + arr[2] + "/";
+		}
+		else {
+			throw new Error("Unknown message from signup service: " + msg);
+		}
+	};
+	
+	connection.onclose = function serverDisconnected() {
+		console.log("Connection to signup service Closed!");
+		
+	};
+	
+	
+	function inputUserNameKeyUp(keyUpEvent) {
+		var charCode = keyUpEvent.charCode || keyUpEvent.keyCode;
+		
+		console.log("charCode=" + charCode);
+		
+		var username = inputUsername.value;
+		
+		usernameAlertDiv.style.display="none";
+		
+		if(username.length < MIN_USERNAME_LENGTH) return alertUsername("Username needs to be at least " + MIN_USERNAME_LENGTH + " characters!");
+		
+		clearTimeout(typingTimer);
+		typingTimer = setTimeout(function() {
+			checkNameAvailability(inputUsername.value);
+		}, 500);
+		
+		if(charCode == CHARCODE_ENTER && inputPassword.value.length >= MIN_PW_LENGTH) createAccount(inputUsername.value, inputPassword.value);
+	}
+	
+	function inputPasswordKeyUp(keyUpEvent) {
+		var charCode = keyUpEvent.charCode || keyUpEvent.keyCode;
+		var password = inputPassword.value;
+		
+		pwAlertDiv.style.display = "none";
+		
+		if(password.length < MIN_PW_LENGTH) alertPassword("The password needs to be at least " + MIN_PW_LENGTH + " characters!");
+		if(!password.match(/[^a-zA-Z]/)) alertPassword("It's a good idea to have special characters in the password!");
+		
+		if(charCode == CHARCODE_ENTER && inputUsername.value.length >= MIN_USERNAME_LENGTH) createAccount(inputUsername.value, inputPassword.value);
+		
+	}
+	
+	
+	function createButtonClick() {
+		var username = inputUsername.value;
+		var password = inputPassword.value;
+		createAccount(username, password, accountCreated);
+	}
+	
+	function checkNameAvailability(username, callback) {
+		
+		if(username.match(/[^a-zA-Z0-9]/)) {
+			alertBox("Username can only contain latin characters a-z, A-Z and numbers 0-9");
+		}
+		else connSend("usernameAvailable:" + username);
+	}
+	
+	function createAccount(username, password, callback) {
+		connSend("createAccount:" + username + ":" + password);
+	}
+	
+	function accountCreated(username) {
+		alert("accountCreated");
+	}
+	
+	function alertGeneralMessage() {
+		generalAlertDiv.innerText = msg;
+		generalAlertDiv.style.display = "inline";
+	}
+	
+	function alertUsername(msg) {
+		usernameAlertDiv.innerText = msg;
+		usernameAlertDiv.style.display = "inline";
+	}
+	
+	function alertPassword(msg) {
+		pwAlertDiv.innerText = msg;
+		pwAlertDiv.style.display = "inline";
+	}
+	
+	function connSend(msg, callback) {
+		
+		if(connection.readyState==SOCKJS_OPEN) {
+			connection.send(msg);
+			if(callback) callback(null);
+		}
+		else {
+			var err = new Error("Not connected to signup service!");
+			err.code = "CONNECTION_CLOSED";
+			if(callback) callback(err);
+			else throw err;
+		}
+		
+	}
+	
+}
--- signup_service.js
+++ signup_service.js
@@ -1,8 +1,10 @@
 /*
 	
-	Lets people signup to get an editor account. 
+	Signup "Micro"-service
 	
-	See jzedit_signup_service.nginx and jzedit_signup.service (systemd)
+	Lets people automatically signup to get an editor account by navigating to /signup/ in a web browser.
+	
+	See etc/jzedit_signup_service.nginx and etc/jzedit_signup.service (systemd)
 	
 	Provides an API via Websockets (SockJS) with the following Methods:
 	usernameAvailable: username
@@ -18,243 +20,266 @@
 */
 
 "use strict";
-var getArg = require("./server/getArg.js");
 
-var UTIL = require("./client/UTIL.js");
-
-var HTTP_PORT = getArg(["p", "port"]) || 8100; 
-if(!UTIL.isNumeric(HTTP_PORT)) throw new Error("HTTP_PORT=" + HTTP_PORT + " is not a numeric value! process arguments=" + process.argv.join(" "))
-
-var HTTP_IP = getArg(["ip", "ip"]) || "127.0.0.1";
-
-var ADMIN_EMAIL = getArg(["admin", "admin", "admin_email"]) || "zeta@zetafiles.org";
-
-var serviceError = "The signup service has a problem!"; // Message to show if there's an internal error
-
-var NO_PW_HASH = getArg(["nopwhash"]);
-var PW_FILE = getArg(["pwfile", "pwfile", "passwordFile"]) || "/etc/jzedit_users";
-
-var LOGLEVEL = getArg(["ll", "loglevel"]) || 7; // Will show log messages lower then or equal to this number
-// Log levels
-var ERROR = 3;
-var WARN = 4;
-var NOTICE = 5;
-var INFO = 6;
-var DEBUG = 7;
-
-var http = require("http");
-var httpServer = http.createServer(handleHttpRequest);
-
-httpServer.listen(HTTP_PORT, HTTP_IP);
-
-var sockJs = require("sockjs");
-var wsServer = sockJs.createServer();
-wsServer.on("connection", sockJsConnection);
-
-wsServer.installHandlers(httpServer, {prefix:'/signup'});
-
-var log; // Using small caps because it looks and feels better
-(function setLogginModule() { // Self calling function to not clutter script scope
-	// Enhanced console.log ...
-	var logModule = require("./server/log.js");
+var RESERVED_USERNAMES = ["JavaScript", "JS", "admin", "root", "webtigerteam", "www", "ftp", "mail", "log", "smtp", "user", "users"]; // Don't allow these usernames
 	
-	logModule.setLogLevel(LOGLEVEL);
-	log = logModule.log;
+	var getArg = require("./server/getArg.js");
 	
-	var logFile = getArg(["lf", "logfile"]) || null; // default: Write to stdout, if specified write to a file
-	
-	if(logFile) logModule.setLogFile(logFile);
-	
-})();
+var HOSTNAME = getArg(["host", "host", "hostname"]) || HTTP_IP; // Same as "server_name" in nginx profile or "VirtualHost" on other web servers
 
-// Overload console.log
-console.log = function() {
-	var msg = arguments[0];
-	for (var i = 1; i < arguments.length; i++) msg += " " + arguments[i];
-	log(msg, 7);
-}
-
-// Overload console.warn
-console.warn = function() {
-	var msg = arguments[0];
-	for (var i = 1; i < arguments.length; i++) msg += " " + arguments[i];
-	log(msg, 4);
-}
-
-function handleHttpRequest(request, response){
+	var UTIL = require("./client/UTIL.js");
 	
-	var IP = request.headers["x-real-ip"] || request.connection.remoteAddress;
+	var HTTP_PORT = getArg(["p", "port"]) || 8100; 
+	if(!UTIL.isNumeric(HTTP_PORT)) throw new Error("HTTP_PORT=" + HTTP_PORT + " is not a numeric value! process arguments=" + process.argv.join(" "))
 	
-	log("HTTP request from IP=" + IP + " to request.url=" + request.url);
+	var HTTP_IP = getArg(["ip", "ip"]) || "127.0.0.1";
 	
-	var responseHeaders = {'Content-Type': 'text/plain; charset=utf-8'};
+	var ADMIN_EMAIL = getArg(["admin", "admin", "admin_email"]) || "zeta@zetafiles.org";
 	
-	response.writeHead(404, "Not found", responseHeaders);
-	response.end("Nothing to see here");
+	var serviceError = "The signup service has a problem!"; // Message to show if there's an internal error
 	
-}
-
-function sockJsConnection(connection) {
+	var NO_PW_HASH = getArg(["nopwhash"]);
+	var PW_FILE = getArg(["pwfile", "pwfile", "passwordFile"]) || "/etc/jzedit_users";
 	
-	var IP = connection.remoteAddress;
+	var LOGLEVEL = getArg(["ll", "loglevel"]) || 7; // Will show log messages lower then or equal to this number
+	// Log levels
+	var ERROR = 3;
+	var WARN = 4;
+	var NOTICE = 5;
+	var INFO = 6;
+	var DEBUG = 7;
 	
-	if(IP == undefined) {
-		// Maybe because the user is connecting via HTTP instead of Websockets!?
-		IP = connection.headers["x-real-ip"];
-		//console.log(JSON.stringify(connection.headers, null, 2));
-	}
-	else {
-		// Update: nginx gives ::ffff:127.0.0.1 !!!?
-		var ipLength = IP.length;
-		var nginxIP = "127.0.0.1";
+	var http = require("http");
+	var httpServer = http.createServer(handleHttpRequest);
+	
+	httpServer.listen(HTTP_PORT, HTTP_IP);
+	
+	var sockJs = require("sockjs");
+	var wsServer = sockJs.createServer();
+	wsServer.on("connection", sockJsConnection);
+	
+	wsServer.installHandlers(httpServer, {prefix:'/signup'});
+	
+	var log; // Using small caps because it looks and feels better
+	(function setLogginModule() { // Self calling function to not clutter script scope
+		// Enhanced console.log ...
+		var logModule = require("./server/log.js");
 		
-		if(IP.substring(ipLength - nginxIP.length) == "127.0.0.1") {
-			// From nginx
-			
-			console.log("connection.headers=" + JSON.stringify(connection.headers));
-			
-			var xRealIp = connection.headers["x-real-ip"]; // X-Real-IP  x-real-ip
-			
-			if(xRealIp == undefined) {
-				log("Unable to get IP address from x-real-ip headers", DEBUG);
-			}
-			
-		}
+		logModule.setLogLevel(LOGLEVEL);
+		log = logModule.log;
+		
+		var logFile = getArg(["lf", "logfile"]) || null; // default: Write to stdout, if specified write to a file
+		
+		if(logFile) logModule.setLogFile(logFile);
+		
+	})();
+	
+	// Overload console.log
+	console.log = function() {
+		var msg = arguments[0];
+		for (var i = 1; i < arguments.length; i++) msg += " " + arguments[i];
+		log(msg, 7);
 	}
 	
-	log("Connection on " + connection.protocol + " from " + IP);
-	
-	connection.on("data", function(message) {
-		
-		log(IP + " => " + UTIL.shortString(message));
-		
-		// message: command:data
-		
-		var command = message.substring(0, message.indexOf(":"));
-		var data = message.substring(message.indexOf(":")+1);
-		
-		if(!command) answer("Unknown command in message: " + message);
-		else if(!data) answer("No data in message: " + message);
-		else if(command == "usernameAvailable") usernameAvailable(data, function available(err, name, isAvailable) {
-			if(err) answer("availableError:" + name + ":" + err);
-			else answer("available:" + name + ":" + isAvailable);
-		});
-		else if(command == "createAccount") createAccount(data, function account(err, username) {
-			if(err) answer("createError:" + username + ":" + err);
-			else answer("created:" + username);
-		});
-		else answer("serviceError:Unknown command: " + command);
-		
-		
-		function answer(str) {
-			log(IP + " <= " + UTIL.shortString(str));
-			connection.write(str);
-		}
-	});
-	
-	
-	function usernameAvailable(username, callback) {
-		var fs = require("fs");
-		
-		var encoding = "utf8";
-		fs.readFile(PW_FILE, encoding, function(err, usersPwString) {
-			if(err) {
-				log("Unable to read PW_FILE=" + PW_FILE + "! " + err.message, ERROR);
-				callback(serviceError);
-				sendAlert(err.message + "\n" + err.stack);
-			}
-			else {
-				console.log("usersPwString:\n" + usersPwString);
-				var users = usersPwString.split(/\r|\r\n/);
-				for (var i=0, name; i<users.length; i++) {
-					name = users[i].substring(0, users[i].indexOf("|"));
-					if(name == username) return callback(null, name, false);
-				}
-				return callback(null, username, true);
-			}
-		});
+	// Overload console.warn
+	console.warn = function() {
+		var msg = arguments[0];
+		for (var i = 1; i < arguments.length; i++) msg += " " + arguments[i];
+		log(msg, 4);
 	}
 	
-	function createAccount(userData, callback) {
-		var username = userData.substring(0, userData.indexOf(","));
-		var password = userData.substring(userData.indexOf(",") + 1);
+	function handleHttpRequest(request, response){
 		
-		var exec = require('child_process').exec;
-		var script = 'adduser.js';
-		exec(script, function adduser(error, stdout, stderr) {
-			if (error) {
-				log("Unable to create username=" + username + "! error=" + error, ERROR);
-				callback(serviceError);
-				sendAlert(error);
-				}
-			else if(stderr) {
-				log("Unable to create username=" + username + "! stderr=" + stderr, ERROR);
-				callback(serviceError);
-				sendAlert(stderr);
-			}
-			else if(stdout) {
-				var check = stdout.match(/User with username=(.*) and password=(.*) successfully added to (.*)/);
-				
-				if(check == null) {
-					log("Unable to create username=" + username + "! stdout=" + stdout, ERROR);
-					callback(serviceError);
-					sendAlert(stdout);
-				}
-				else if(check[2] == username && check[3] == password && check[4] == PW_FILE) {
-					log("Account username=" + username + "! successfully created!");
-					callback(null, username);
-				}
-				else {
-					log("Problem when creating username=" + username + "! stdout=" + stdout, ERROR);
-					callback(serviceError);
-					sendAlert(stdout);
-				}
-			}
-			else {
-				log("Problem when creating username=" + username + "! Exec script=" + script + " did not return anyting! arguments=" + JSON.stringify(arguments), ERROR);
-				callback(serviceError);
-				sendAlert(stdout);
-			}
-			
-		});
+		var IP = request.headers["x-real-ip"] || request.connection.remoteAddress;
+		
+		log("HTTP request from IP=" + IP + " to request.url=" + request.url);
+		
+		var responseHeaders = {'Content-Type': 'text/plain; charset=utf-8'};
+		
+		response.writeHead(404, "Not found", responseHeaders);
+		response.end("Nothing to see here");
+		
 	}
 	
-	
-}
-
-function sendAlert(text) {
-	
-	sendMail("errors@webtigerteam.com", ADMIN_EMAIL, "JZedit cloud editor signup problems", text);
-	
-}
-
-function sendMail(from, to, subject, text) {
-	
-	log("Sending mail from=" + from + " to=" + to + " subject=" + subject + " text=" + text);
-	
-	var nodemailer = require('nodemailer');
-	var smtpTransport = require('nodemailer-smtp-transport');
-	
-	var mailSettings = {};
-	mailSettings.port = 25;
-	mailSettings.host = "127.0.0.1"; // Need to allow relay from 127.0.0.1 !
-	
-	
-	var transporter = nodemailer.createTransport(smtpTransport(mailSettings));
-	
-	transporter.sendMail({
-		from: from,
-		to: to,
-		subject: subject,
-		text: text
+	function sockJsConnection(connection) {
 		
-	}, function(error, info){
-		if(error) {
-			throw new Error(error);
+		var IP = connection.remoteAddress;
+		
+		if(IP == undefined) {
+			// Maybe because the user is connecting via HTTP instead of Websockets!?
+			IP = connection.headers["x-real-ip"];
+			//console.log(JSON.stringify(connection.headers, null, 2));
 		}
 		else {
-			log("Mail sent: " + info.response);
+			// Update: nginx gives ::ffff:127.0.0.1 !!!?
+			var ipLength = IP.length;
+			var nginxIP = "127.0.0.1";
+			
+			if(IP.substring(ipLength - nginxIP.length) == "127.0.0.1") {
+				// From nginx
+				
+				console.log("connection.headers=" + JSON.stringify(connection.headers));
+				
+				var xRealIp = connection.headers["x-real-ip"]; // X-Real-IP  x-real-ip
+				
+				if(xRealIp == undefined) {
+					log("Unable to get IP address from x-real-ip headers", DEBUG);
+				}
+				
+			}
 		}
-	});
+		
+		log("Connection on " + connection.protocol + " from " + IP);
+		
+		connection.on("data", function(message) {
+			
+			log(IP + " => " + UTIL.shortString(message));
+			
+			// message: command:data
+			
+			var command = message.substring(0, message.indexOf(":"));
+			var data = message.substring(message.indexOf(":")+1);
+			
+			if(!command) answer("Unknown command in message: " + message);
+			else if(!data) answer("No data in message: " + message);
+			else if(command == "usernameAvailable") usernameAvailable(data, function available(err, name, isAvailable) {
+				if(err) answer("availableError:" + name + ":" + err);
+				else answer("available:" + name + ":" + isAvailable);
+			});
+			else if(command == "createAccount") createAccount(data, function account(err, username) {
+			if(err) answer("createError:" + username + ":" + HOSTNAME + ":" + err);
+			else answer("created:" + username + ":" + HOSTNAME);
+			});
+			else answer("serviceError:Unknown command: " + command);
+			
+			
+			function answer(str) {
+				log(IP + " <= " + UTIL.shortString(str));
+				connection.write(str);
+			}
+		});
+		
+		
+		function usernameAvailable(username, callback) {
+			var fs = require("fs");
+			
+		if(RESERVED_USERNAMES.indexOf(username) != -1) return callback(null, username, false);
+			
+			var encoding = "utf8";
+			
+			fs.readFile(PW_FILE, encoding, function(err, usersPwString) {
+				
+				if(err) {
+					log("Unable to read PW_FILE=" + PW_FILE + "! " + err.message, ERROR);
+					callback(serviceError);
+					sendAlert(err.message + "\n" + err.stack);
+				}
+				else {
+					console.log("usersPwString:\n" + usersPwString);
+					var users = usersPwString.split(/\r|\r\n/);
+					for (var i=0, name; i<users.length; i++) {
+						name = users[i].substring(0, users[i].indexOf("|"));
+						if(name == username) return callback(null, name, false);
+					}
+					
+					// Also check for system users
+					fs.readFile("/etc/passwd", encoding, function(err, usersPwString) {
+						console.log("usersPwString:\n" + usersPwString);
+						var users = usersPwString.split(/\r|\r\n/);
+						for (var i=0, name; i<users.length; i++) {
+							name = users[i].substring(0, users[i].indexOf(":"));
+							if(name == username) return callback(null, name, false);
+						}
+						
+						return callback(null, username, true);
+						
+					});
+					
+					
+				}
+			});
+		}
+		
+		function createAccount(userData, callback) {
+			var username = userData.substring(0, userData.indexOf(","));
+			var password = userData.substring(userData.indexOf(",") + 1);
+			
+			var exec = require('child_process').exec;
+			var script = 'adduser.js';
+			exec(script, function adduser(error, stdout, stderr) {
+				if (error) {
+					log("Unable to create username=" + username + "! error=" + error, ERROR);
+					callback(serviceError);
+					sendAlert(error);
+				}
+				else if(stderr) {
+					log("Unable to create username=" + username + "! stderr=" + stderr, ERROR);
+					callback(serviceError);
+					sendAlert(stderr);
+				}
+				else if(stdout) {
+					var check = stdout.match(/User with username=(.*) and password=(.*) successfully added to (.*)/);
+					
+					if(check == null) {
+						log("Unable to create username=" + username + "! stdout=" + stdout, ERROR);
+						callback(serviceError);
+						sendAlert(stdout);
+					}
+					else if(check[2] == username && check[3] == password && check[4] == PW_FILE) {
+						log("Account username=" + username + "! successfully created!");
+						callback(null, username);
+					}
+					else {
+						log("Problem when creating username=" + username + "! stdout=" + stdout, ERROR);
+						callback(serviceError);
+						sendAlert(stdout);
+					}
+				}
+				else {
+					log("Problem when creating username=" + username + "! Exec script=" + script + " did not return anyting! arguments=" + JSON.stringify(arguments), ERROR);
+					callback(serviceError);
+					sendAlert(stdout);
+				}
+				
+			});
+		}
+		
+		
+	}
 	
-}
+	function sendAlert(text) {
+		
+		sendMail("errors@webtigerteam.com", ADMIN_EMAIL, "JZedit cloud editor signup problems", text);
+		
+	}
+	
+	function sendMail(from, to, subject, text) {
+		
+		log("Sending mail from=" + from + " to=" + to + " subject=" + subject + " text=" + text);
+		
+		var nodemailer = require('nodemailer');
+		var smtpTransport = require('nodemailer-smtp-transport');
+		
+		var mailSettings = {};
+		mailSettings.port = 25;
+		mailSettings.host = "127.0.0.1"; // Need to allow relay from 127.0.0.1 !
+		
+		
+		var transporter = nodemailer.createTransport(smtpTransport(mailSettings));
+		
+		transporter.sendMail({
+			from: from,
+			to: to,
+			subject: subject,
+			text: text
+			
+		}, function(error, info){
+			if(error) {
+				throw new Error(error);
+			}
+			else {
+				log("Mail sent: " + info.response);
+			}
+		});
+		
+	}
