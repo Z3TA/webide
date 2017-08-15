17188636facd2395bf2a28a63abc854b8de9e430
Z <zeta@zetafiles.org>
2017-08-15 23:14:41 +0200

    consitent camelCase and more info

--- etc/userdir_skeleton/nodejs/http_server_example.js
+++ etc/userdir_skeleton/nodejs/http_server_example.js
@@ -1,11 +1,11 @@
 
 var http = require('http')
-var unixSocket = "/home/test123/sock/_http1";
+var unixSocket = "/sock/_http1";
 
 // We need the group to have write access for the unix socket to work
-var newmask = parseInt("0007", 8); // four digits, last three mask, ex: 0o027 ==> 750 file permissions
-var oldmask = process.umask(newmask);
-console.log("Changed umask from " + oldmask.toString(8) + " to " + newmask.toString(8));
+var newMask = parseInt("0007", 8); // four digits, last three mask, ex: 0o027 ==> 750 file permissions
+var oldMask = process.umask(newMask);
+console.log("Changed umask from " + oldMask.toString(8) + " to " + newMask.toString(8));
 
 
 var httpServer = http.createServer(requestHandler)
@@ -14,7 +14,7 @@ httpServer.on("listening", notifyListening);
 httpServer.listen(unixSocket);
 
 function requestHandler(request, response) {
-	console.log(request.url);
+	console.log("Request to " + request.url + " from " + (request.headers["x-real-ip"] || request.connection.remoteAddress));
 	response.end('Hello from Node.js HTTP Server!');
 }
 
@@ -30,5 +30,5 @@ function httpServerError(err) {
 }
 
 function notifyListening() {
-	console.log("httpServer is listening on " + unixSocket);
+	console.log("Listening on http://" + process.env.myName + ".webide.se/" + unixSocket.split("/")[2]);
 }
