f1f929dae0d13fa390dd9bd9d5f0b677def3e101
Z <zeta@zetafiles.org>
2017-08-21 21:57:52 +0200

    "just in case"

--- removeuser.js
+++ removeuser.js
@@ -91,6 +91,14 @@ unlink("/etc/apparmor.d/home." + username + ".usr.bin.hg");
 
 unlink("/usr/bin/nodejs_" + username);
 
+// We don't want to accidently mess with any of these, so just in case we are doing some debugging
+umount("/home/" + username + "/dev/", true);
+umount("/home/" + username + "/lib/", true);
+umount("/home/" + username + "/lib64/", true);
+umount("/home/" + username + "/usr/", true);
+umount("/home/" + username + "/etc/", true);
+umount("/home/" + username + "/run/", true);
+
 
 umount("/home/" + username + "/dev/urandom");
 umount("/home/" + username + "/lib");
@@ -99,6 +107,7 @@ umount("/home/" + username + "/usr/lib");
 umount("/home/" + username + "/usr/bin/hg");
 umount("/home/" + username + "/usr/bin/python");
 umount("/home/" + username + "/usr/bin/nodejs");
+
 
 child_process.exec('userdel -r -f ' + username, function execAddUser(err, stdout, stderr) {
 	if (err) throw err;
@@ -124,18 +133,20 @@ function unlink(path) {
 	}
 }
 
-function umount(path) {
+function umount(path, ignoreErrors) {
 	var child_process = require("child_process");
 	try {
 		child_process.execSync("umount " + path).toString(ENCODING);
 	}
 	catch(err) {
+		if(!ignoreErrors) {
 		if(err.message.indexOf("umount: " + path + ": not mounted") != -1
 		|| err.message.indexOf("umount: " + path + ": mountpoint not found") != -1 ) console.warn(err.message);
 		else {
 			//console.log("*" + err.message.trim() + "*");
 			throw err;
 		}
+	}
 	}
 }
 
