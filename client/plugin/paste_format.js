deb3c7ecaf0360b5606a9a56882cf8728d2c0a47
Z <zeta@zetafiles.org>
2017-09-13 10:18:16 +0200

    Don't remove spaces in plain text files

--- client/plugin/paste_format.js
+++ client/plugin/paste_format.js
@@ -17,6 +17,8 @@
 		//console.log("lineBreak=" + EDITOR.currentFile.lineBreak.replace("\r", "CR").replace("\n", "LF"));
 		text = text.replace(/\r/g, "");
 		
+		if(file.mode == "text") return text; // Don't remove spaces
+		
 		//console.log("text=" + UTIL.lbChars(text));
 		
 		var lines = text.split("\n");
