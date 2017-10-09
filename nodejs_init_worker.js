5969b23779c40b49cb6e9e6937e6150941982b0d
Z <zeta@zetafiles.org>
2017-10-09 10:33:36 +0200

    working on nodejs init worker ...

--- nodejs_init_worker.js
+++ nodejs_init_worker.js
@@ -41,17 +41,23 @@ log("Starting nodeinit");
   var DEFAULT_EMAIL = "zeta@zetafiles.org"; // Errors with This script is sent here
 var DEFAULT_PATH = "/tank/nodejs/";
 
-var ARG = getArguments(process.argv);
+var getArg = require("./getArg.js");
 
-	var PATH = ARG["path"];
-	var EMAIL = ARG["email"];
+var PATH = getArg(["path"]) || process.env.homeDir;
+var EMAIL = getArg(["email"]) || process.env.email;
 
-var CHILD = {}; // Holds references to the child processes
 
-var SHUTDOWN = false;
 
+	var CHILD = {}; // Holds references to the child processes
+	
+	var SHUTDOWN = false;
+	
+	var UTC = false; // If set to true, use GMT+0 on time stamps
+	
+	
+	
 	if(!PATH) {
-	return initError(new Error("No path specified. Use argument: --path=/path/to/folder/!"));
+		return initError(new Error("No path specified. Use argument: --path=/path/to/folder/!"));
 	}
 	
 	if(!EMAIL) {
@@ -59,110 +65,123 @@ var SHUTDOWN = false;
 		EMAIL= DEFAULT_EMAIL;
 	}
 	
-if(ARG["runtests"] !== undefined) {
-	runTests();
-	}
-else {
+// Require non-native modules before entering chroot
+	var NODE_MAILER = require('nodemailer');
+	var SMTP_TRANSPORT = require('nodemailer-smtp-transport');
 	
- findScripts(PATH, function(scripts) {
-		
-		scripts.forEach(fork);
-		
-	});
-	}
-
-process.on("SIGINT", function () {
-	log("Received SIGINT");
-	
-	if(SHUTDOWN) {
-		// Second time receiving the SIGINT, kill all children and exit
-		
-		log("Killing all child processes!");
-		
-		for(var name in CHILD) CHILD[name].kill('SIGKILL');
-		
-		process.exit();
-		
-	}
-	
-	SHUTDOWN = true;
-	
-	// Close all child processes ...
-	
-	var closed = [];
-	
-	for(var name in CHILD) {
-		closeChild(CHILD[name]);
-		log("Closing " + name);
-		closed.push(name);
-	}
-	
-	if(closed.length > 0) {
-		// Tell what process was killed
-	sendMail(EMAIL, "Killing processes due to SIGINT", "The daemon master process received a SIGINT and killed the following daemons:\n * " + closed.join("\n * "), undefined, function() {
-		process.exit();
-	});
-	}
-	else process.exit();
-	
-});
-
-function closeChild(childProcess) {
-	// Allow the process to gracefully shut down
-	
-	if(childProcess.connected) childProcess.disconnect();
-	
-	childProcess.kill('SIGTERM');
-	childProcess.kill('SIGINT');
-	childProcess.kill('SIGQUIT');
-	childProcess.kill('SIGHUP');
-	}
 
 
-function getArguments(argArr) {
-	var args = {};
-	
-	for(var i=2, arg; i<argArr.length; i++) {
-		if(argArr[i].substr(0, 2) == "--") {
-			var arr = argArr[i].substr(2).split("=");
-			var name = arr[0];
-			var value = arr[1] || "";
+
+	function main() {
+		
+	if(getArg(["runtests"]) !== undefined) {
+		runTests();
+	}
+	else {
+		
+		findScripts(PATH, function(scripts) {
 			
-			args[name] = value;
-		}
+			scripts.forEach(fork);
+			
+		});
 	}
-	return args;
-}
-
-
-function findScripts(pathToFolder, callback) {
-	var fs = require("fs");
-	var path = require("path");
+		
+		process.on("SIGINT", function() {
+			log("Received SIGINT");
+			shutdownInitWorker();
+		});
 	
-	var scripts = []; // Scripts found
 	
-	var foldersToLookIn = 0;
-	var filesToStat = 0;
+	process.on('message', function commandMessage(message) {
+		
+		if(message == undefined) throw new Error("User worker message=" + message);
+		
+		if(message.restart) restart(message.restart);
+		else if(message.stop) stop(message.stop);
+		else if(message.shutdown) shutdownInitWorker();
+		else throw new Error("Unknown message=" + JSON.stringify(message) : message);
+		
+	});
 	
-	log("Reading directory pathToFolder=" + pathToFolder, 7);
+	}
 	
-	fs.readdir(pathToFolder, function readdir(err, folderItems) {
-		if(err) {
-			return initError(err);
+	function shutdownInitWorker() {
+		
+		
+		if(SHUTDOWN) {
+			// Second time receiving the SIGINT, kill all children and exit
+			
+			log("Killing all child processes!");
+			
+			for(var name in CHILD) CHILD[name].kill('SIGKILL');
+			
+			process.exit();
+			
 		}
-		if(folderItems.length == 0) initError( new Error("No files found in pathToFolder=" + pathToFolder) ); 
-		else {
-			folderItems.forEach(function(fileName) {
-				
-				if(fileName == "nodeinit") return; // Ignore ourselves
-				
-				var filePath = path.join(pathToFolder, fileName)
-				
-				if(fileName.indexOf("�") != -1) log("Encoding problem in filePath=" + filePath, 4);
-				
-				stat(fileName, filePath);
-				
+		
+		SHUTDOWN = true;
+		
+		// Close all child processes ...
+		
+		var closed = [];
+		
+		for(var name in CHILD) {
+			closeChild(CHILD[name]);
+			log("Closing " + name);
+			closed.push(name);
+		}
+		
+		if(closed.length > 0) {
+			// Tell what process was killed
+			sendMail(EMAIL, "Killing processes due to SIGINT", "The daemon master process received a SIGINT and killed the following daemons:\n * " + closed.join("\n * "), undefined, function() {
+				process.exit();
 			});
+		}
+		else process.exit();
+		
+	}
+	
+	
+	function closeChild(childProcess) {
+		// Allow the process to gracefully shut down
+		
+		if(childProcess.connected) childProcess.disconnect();
+		
+		childProcess.kill('SIGTERM');
+		childProcess.kill('SIGINT');
+		childProcess.kill('SIGQUIT');
+		childProcess.kill('SIGHUP');
+	}
+	
+	
+	function findScripts(pathToFolder, callback) {
+		var fs = require("fs");
+		var path = require("path");
+		
+		var scripts = []; // Scripts found
+		
+		var foldersToLookIn = 0;
+		var filesToStat = 0;
+		
+		log("Reading directory pathToFolder=" + pathToFolder, 7);
+		
+		fs.readdir(pathToFolder, function readdir(err, folderItems) {
+			if(err) {
+				return initError(err);
+			}
+			if(folderItems.length == 0) initError( new Error("No files found in pathToFolder=" + pathToFolder) ); 
+			else {
+				folderItems.forEach(function(fileName) {
+					
+					if(fileName == "nodeinit") return; // Ignore ourselves
+					
+					var filePath = path.join(pathToFolder, fileName)
+					
+					if(fileName.indexOf("�") != -1) log("Encoding problem in filePath=" + filePath, 4);
+					
+					stat(fileName, filePath);
+					
+				});
 			}
 			
 			checkDone();
@@ -213,22 +232,22 @@ function findScripts(pathToFolder, callback) {
 		
 	}
 	
-	function fork(scriptPath) {
+	function startScript(scriptPath, logFile) {
 		
-	var scriptName = getFileNameFromPath(scriptPath);
-	
-	log("Found scriptName=" + scriptName + " scriptPath=" + scriptPath, 7);
-	
+		var scriptName = getFileNameFromPath(scriptPath);
+		
+		log("Found scriptName=" + scriptName + " scriptPath=" + scriptPath, 7);
+		
 		var waitRestart = [2000,5000,10000,30000,60000,1800000];
-	var waitKill = 1000; // How long to wait from SIGHUB to SIGKILL
+		var waitKill = 1000; // How long to wait from SIGHUB to SIGKILL
 		
 		var restarts = 0;
 		var respawnTimer;
 		var stdHistory = []; // History of stdout and stderr messages
 		var historyLimit = 100;
-	var isRestarting = true; // Prevent killing a process that is about to respawn
-	var resetRestartsTimer;
-	
+		var isRestarting = true; // Prevent killing a process that is about to respawn
+		var resetRestartsTimer;
+		
 		var cp = require('child_process');
 		var arg = [];
 		var opt = {silent: true};
@@ -236,212 +255,170 @@ function findScripts(pathToFolder, callback) {
 		
 		respawn(); // Starts the child process
 		
-	var watcherTimer;
-		var chokidar = require('chokidar');
-		var watcher = chokidar.watch(scriptPath);
-		watcher.on('change', function(path) {
-			log("Script changed: " + scriptPath);
-			
-			restarts = 0;
-			
-		if(isRestarting) {
-			// Wait one second for all writes to finish, so we don't load a corrupt file
-			clearTimeout(watcherTimer);
-			watcherTimer = setTimeout(function() {
-				if(isRestarting) {
-					// Now restart it right away
-					respawn();
-				}
-			}, 1000);
-		}
-		else {
-			
-			closeChild(childProcess);
-			
-			setTimeout(function() {
-				// Kill the process if it's still running
-				if(!isRestarting) childProcess.kill('SIGKILL');
-				
-				}, waitKill);
-			}
-		});
+		var logStream = fs.createWriteStream(logFilePath, {'flags': 'a'});
+		logStream.write(myDate() + ": Started!");
 		
 		
 		function respawn() {
-		
-		clearTimeout(respawnTimer);
-		
-		if(SHUTDOWN) return;
-		
-		if(childProcess) {
-			// Make sure the old one is really really dead
-		childProcess.kill('SIGKILL');
-		if(childProcess.connected) childProcess.disconnect();
-		}
-		
+			
+			clearTimeout(respawnTimer);
+			
+			if(SHUTDOWN) return;
+			
+			if(childProcess) {
+				// Make sure the old one is really really dead
+				childProcess.kill('SIGKILL');
+				if(childProcess.connected) childProcess.disconnect();
+			}
+			
 			log("Starting: " + scriptPath, 7);
 			
 			stdHistory.length = 0; // Reset the history
 			
-		CHILD[scriptName] = cp.fork(scriptPath, arg, opt);
+			CHILD[scriptName] = cp.fork(scriptPath, arg, opt);
 			
-		childProcess = CHILD[scriptName];
-		
+			childProcess = CHILD[scriptName];
+			
+			logStream = fs.createWriteStream(logFilePath, {'flags': 'a'});
+			logStream.write(myDate() + ": Restarted!");
+			
 			// Attach event listeners
 			childProcess.stdout.on('data', stdout);
 			childProcess.stderr.on('data', stderr);
-		childProcess.on('close', closeStdioStreams);
-		childProcess.on('exit', childProcessEnded);
-		
-		isRestarting= false;
+			childProcess.on('close', closeStdioStreams);
+			childProcess.on('exit', childProcessEnded);
+			
+			isRestarting= false;
 			
 		}
 		
 		function stdout(data) {
-		var txtArr = data.toString("utf8").trim().split("\n");
-		
-		for(var i=0; i<txtArr.length; i++) {
-			log(scriptName + ": " + txtArr[i], 7);
-		historyAdd(txtArr[i]);
+			var txtArr = data.toString("utf8").trim().split("\n");
+			
+			for(var i=0; i<txtArr.length; i++) {
+				log(scriptName + ": " + txtArr[i], 7);
+				historyAdd(txtArr[i]);
+				logStream.write(txtArr[i] + "\n");
+			}
+			
 		}
 		
-	}
-	
-	function stderr(data) {
-		var txtArr = data.toString("utf8").trim().split("\n");
-		
-		for(var i=0; i<txtArr.length; i++) {
-			log(scriptName + ": " + txtArr[i], 3);
-			historyAdd(txtArr[i]);
-		}
-		
-		// Send the error message via email
-		
-		var firstLineInErrMsg = txtArr[0];
-		
-		sendMail(EMAIL, scriptName + ": " + txtArr[4], data);
-		
+		function stderr(data) {
+			var txtArr = data.toString("utf8").trim().split("\n");
+			
+			for(var i=0; i<txtArr.length; i++) {
+				log(scriptName + ": " + txtArr[i], 3);
+				historyAdd(txtArr[i]);
+				logStream.write(txtArr[i] + "\n");
+			}
+			
+			// Send the error message via email
+			
+			var firstLineInErrMsg = txtArr[0];
+			
+			sendMail(EMAIL, scriptName + ": " + txtArr[4], data);
+			
 			// Log the error
 			
-		log(scriptName + ": " + txtArr[0] + " " + txtArr[4], 3);
-		
+			log(scriptName + ": " + txtArr[0] + " " + txtArr[4], 3);
+			
 		}
-	
-	
-	function closeStdioStreams(code, signal) {
 		
-		log(scriptName + " closed stdio streams! code=" + code + " signal=" + signal);
 		
-	}
-	
-	function childProcessEnded(code, signal) {
-		var finalExit = (code !== null);
-		
-		log(scriptName + " ended! code=" + code + " signal=" + signal + " finalExit=" + finalExit);
-		
-		// NodeJS errors will have a code, but if the process is killed via a signal, we wont get a final exit
-		// So always restart!
-		
-		if(!finalExit) closeChild(childProcess); // Make sure it's really dead, and disconnect from it
-		// (but do not send SIGKILL because we want to give it a chance to gracefully shut down)
-		
-		exit(code, signal);
-		
-	}
-	
-	
-	function exit(code, signal) {
-		
-		isRestarting = true;
-		
-		if(SHUTDOWN) return;
-		
-		var waitForRespawn = 0;
-		if(restarts < waitRestart.length-1) waitForRespawn = waitRestart[restarts];
-		else waitForRespawn = waitRestart[waitRestart.length-1];
-		
-		sendMail(EMAIL, scriptName + ": Exit code=" + code + " signal=" + signal, stdHistory.join("\n"));
-		
-		log("Waiting " + waitForRespawn + "ms to restart: " + scriptPath, 7);
-		
-		clearTimeout(resetRestartsTimer);
-		clearTimeout(respawnTimer);
-		
-		respawnTimer = setTimeout(function() {
+		function closeStdioStreams(code, signal) {
 			
-			restarts++;
+			log(scriptName + " closed stdio streams! code=" + code + " signal=" + signal);
 			
-			respawn();
-			
-			resetRestartsTimer = setTimeout(function() {
-				// Reset the restarts counter if the service has been running for more then 60 seconds ...
-				restarts = 0;
-			}, 60000);
-			
-		}, waitForRespawn);
-		
-	}
-	
-	function historyAdd(msg) {
-		
-		stdHistory.push(myDate() + ": " + msg);
-		
-		if(stdHistory.length > historyLimit) stdHistory.shift();
-		
-		
-		function myDate() {
-			var d = new Date();
-			
-			var hour = addZero(d.getHours());
-			var minute = addZero(d.getMinutes());
-			var second = addZero(d.getSeconds());
-			
-			var day = addZero(d.getDate());
-			var month = addZero(1+d.getMonth());
-			var year = d.getFullYear();
-			
-			return year + "-" + month + "-" + day + " (" + hour + ":" + minute + ":" + second + ")";
-			
-			function addZero(n) {
-				if(n < 10) return "0" + n;
-					else return n;
-				}
-			}
 		}
+		
+		function childProcessEnded(code, signal) {
+			var finalExit = (code !== null);
+			
+			log(scriptName + " ended! code=" + code + " signal=" + signal + " finalExit=" + finalExit);
+			
+			// NodeJS errors will have a code, but if the process is killed via a signal, we wont get a final exit
+			// So always restart!
+			
+			if(!finalExit) closeChild(childProcess); // Make sure it's really dead, and disconnect from it
+			// (but do not send SIGKILL because we want to give it a chance to gracefully shut down)
+			
+			exit(code, signal);
+			
 		}
-	
-function getFileNameFromPath(path) {
-	var file = "";
-	if(path.indexOf("/") > -1) {
-		file = path.substr(path.lastIndexOf('/')+1);
+		
+		
+		function exit(code, signal) {
+			
+			logStream.end(myDate() + ": Exit: code=" + code + " signal=" + signal + "");
+			
+			isRestarting = true;
+			
+			if(SHUTDOWN) return;
+			
+			var waitForRespawn = 0;
+			if(restarts < waitRestart.length-1) waitForRespawn = waitRestart[restarts];
+			else waitForRespawn = waitRestart[waitRestart.length-1];
+			
+			sendMail(EMAIL, scriptName + ": Exit code=" + code + " signal=" + signal, stdHistory.join("\n"));
+			
+			log("Waiting " + waitForRespawn + "ms to restart: " + scriptPath, 7);
+			
+			clearTimeout(resetRestartsTimer);
+			clearTimeout(respawnTimer);
+			
+			respawnTimer = setTimeout(function() {
+				
+				restarts++;
+				
+				respawn();
+				
+				resetRestartsTimer = setTimeout(function() {
+					// Reset the restarts counter if the service has been running for more then 60 seconds ...
+					restarts = 0;
+				}, 60000);
+				
+			}, waitForRespawn);
+			
+		}
+		
+		function historyAdd(msg) {
+			
+			stdHistory.push(myDate() + ": " + msg);
+			
+			if(stdHistory.length > historyLimit) stdHistory.shift();
+			
+		}
 	}
-	else {
-		// Assume \ is the folder separator
-		file = path.substr(path.lastIndexOf('\\')+1);
+	
+	function getFileNameFromPath(path) {
+		var file = "";
+		if(path.indexOf("/") > -1) {
+			file = path.substr(path.lastIndexOf('/')+1);
+		}
+		else {
+			// Assume \ is the folder separator
+			file = path.substr(path.lastIndexOf('\\')+1);
+		}
+		
+		var dot = file.indexOf(".");
+		
+		if(dot > 0) file = file.substr(0, dot);
+		
+		return file;
 	}
-	
-	var dot = file.indexOf(".");
-	
-	if(dot > 0) file = file.substr(0, dot);
-	
-	return file;
-}
 	
 	function sendMail(to, subject, text, from, callback) {
 		
 		if(from == undefined) from = "mailform@zetafiles.org";
 		
-	text = String(text);
-	
-	log("Sending mail from=" + from + " to=" + to + " subject=" + subject + " text=\n********************************************\n* " + text.replace(/\n/g, "\n* ") + "\n********************************************\n", 7);
-	
-		var nodemailer = require('nodemailer');
-		var smtpTransport = require('nodemailer-smtp-transport');
+		text = String(text);
 		
-		var transporter = nodemailer.createTransport(smtpTransport({
+		log("Sending mail from=" + from + " to=" + to + " subject=" + subject + " text=\n********************************************\n* " + text.replace(/\n/g, "\n* ") + "\n********************************************\n", 7);
+		
+		var transporter = NODE_MAILER.createTransport(SMTP_TRANSPORT({
 			port: 25,
-		host: "zetafiles.org",
-		auth: {
+			host: "zetafiles.org",
+			auth: {
 				user: "mailform@zetafiles.org",
 				pass: "oijafs8419jifg555757fsdds3fhkl4edgggsd"
 			}
@@ -454,50 +431,57 @@ function getFileNameFromPath(path) {
 			text: text
 			
 		}, function(err, info){
-		
-		if(err){
+			
+			if(err){
 				log("Failed to send send e-mail! " + err.message, 2);
 			}
-		else{
-			log("Mail sent: " + info.response);
+			else{
+				log("Mail sent: " + info.response);
 			}
+			
+			if(callback) callback(err, info);
+			
+		});
+	}
+	
+	
+	function initError(err) {
 		
-		if(callback) callback(err, info);
+		// Get the proper position for this error
+		var callsites = callsite();
+		var callstack = "";
+		var firstSite = callsites[1];
+		var position = (firstSite.getFunctionName() || '(anonymous function)') + "@" + firstSite.getFileName() + ":" + firstSite.getLineNumber();
+		for(var i=1; i<callsites.length; i++) callstack += (callsites[i].getFunctionName() || 'anonymous') + "@" + callsites[i].getFileName() + ":" + callsites[i].getLineNumber() + "\n";
 		
-	});
+		//console.log(callstack);
+		
+		log(position + "\n    " + err.message, 3);
+		
+		sendMail(DEFAULT_EMAIL, "Init error: " + err.message, "Message: " + err.message + "\n\nStack:\n" + callstack + "\n\n Arguments:\n" + process.argv.join("\n"));
 	}
-
-
-function initError(err) {
 	
-	// Get the proper position for this error
-	var callsites = callsite();
-	var callstack = "";
-	var firstSite = callsites[1];
-	var position = (firstSite.getFunctionName() || '(anonymous function)') + "@" + firstSite.getFileName() + ":" + firstSite.getLineNumber();
-	for(var i=1; i<callsites.length; i++) callstack += (callsites[i].getFunctionName() || 'anonymous') + "@" + callsites[i].getFileName() + ":" + callsites[i].getLineNumber() + "\n";
 	
-	//console.log(callstack);
-	
-	log(position + "\n    " + err.message, 3);
-	
-	sendMail(DEFAULT_EMAIL, "Init error: " + err.message, "Message: " + err.message + "\n\nStack:\n" + callstack + "\n\n Arguments:\n" + process.argv.join("\n"));
-}
-
-
-function log(msg, level) {
-	if(level == undefined) level = 6;
-	
-	if(process.stdout.isTTY) {
-		console.log(myDate() + " " + msg);
+	function log(msg, level) {
+		if(level == undefined) level = 6;
+		
+		if(process.stdout.isTTY) {
+			console.log(myDate() + " " + msg);
+		}
+		else {
+			// Probably running under systemd
+			console.log("<" + level + ">" + msg);
+		}
 	}
-	else {
-		// Probably running under systemd
-		console.log("<" + level + ">" + msg);
-	}
+	
 	
 	function myDate() {
 		var d = new Date();
+		
+		if(UTC) {
+			var timezone =  d.getTimezoneOffset() // difference in minutes from GMT
+			d.setTime(d.getTime() + (timezone*60*1000));
+		}
 		
 		var hour = addZero(d.getHours());
 		var minute = addZero(d.getMinutes());
@@ -516,21 +500,20 @@ function log(msg, level) {
 	}
 	
 	
-}
-
-function runTests() {
-	sendMail(EMAIL, "test", "test text");
-}
-
-function callsite() {
-	var orig = Error.prepareStackTrace;
-	Error.prepareStackTrace = function(_, stack){ return stack; };
-	var err = new Error;
-	Error.captureStackTrace(err, callsite);
-	var stack = err.stack;
-	Error.prepareStackTrace = orig;
-	return stack;
-}
-
-
-
+	function runTests() {
+		sendMail(EMAIL, "test", "test text");
+	}
+	
+	function callsite() {
+		var orig = Error.prepareStackTrace;
+		Error.prepareStackTrace = function(_, stack){ return stack; };
+		var err = new Error;
+		Error.captureStackTrace(err, callsite);
+		var stack = err.stack;
+		Error.prepareStackTrace = orig;
+		return stack;
+	}
+	
+main();
+	
+	
