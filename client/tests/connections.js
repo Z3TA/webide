53445953442882c1bb5c990fe67d9b6f1efa90ea
Z <zeta@zetafiles.org>
2017-10-16 16:32:36 +0200

    Instructions on how to setup sftp

--- client/tests/connections.js
+++ client/tests/connections.js
@@ -9,6 +9,43 @@
 		var testFile = "testReadWrite.txt";
 		var testText = "abc123\n";
 		
+		/*
+			Setting up the sftp test server:
+			1. Create sftpusers group.
+			# sudo groupadd sftpusers
+			
+			2. Comment out setting disabling SFTP access from sshd config file.
+			# sudo sed -i "s/Subsystem sftp \/usr\/lib\/openssh\/sftp-server/#Subsystem sftp \/usr\/lib\/openssh\/sftp-server/" /etc/ssh/sshd_config
+			
+			3. Open sshd config file sudo nano /etc/ssh/sshd_config, add below snippet
+			# sudo nano /etc/ssh/sshd_config
+			
+			#enable sftp
+			Subsystem sftp internal-sftp
+			
+			Match Group sftpusers
+			ChrootDirectory %h #set the home directory
+			ForceCommand internal-sftp
+			X11Forwarding no
+			AllowTCPForwarding no
+			PasswordAuthentication yes
+			
+			4. Restart ssh.
+			# service ssh restart
+			
+			5. Create a user
+			# sudo adduser sftptest
+			# sudo usermod -g sftpusers sftptest
+			# sudo usermod -s /bin/nologin sftptest
+			# sudo chown root:sftptest /home/sftptest
+			# sudo chmod 755 /home/sftptest
+			# sudo mkdir /home/sftptest/uploads
+			# sudo chown sftptest:sftptest /home/sftptest/uploads
+			# sudo chmod 755 /home/sftptest/uploads
+			
+		*/
+		
+		
 		CLIENT.cmd("connect", {protocol: protocol, serverAddress: serverAddress, user: "sftptest", passw: "12345"}, function(err, json) {
 			if(err) throw err
 			else {
