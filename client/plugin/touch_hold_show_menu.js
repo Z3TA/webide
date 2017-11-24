6b9c1ea9c9f7e5b5f56d5831c148463702093f9e
Z <zeta@zetafiles.org>
2017-11-24 10:04:09 +0100

    Don't show menu when dragging (selecting text)

--- client/plugin/touch_hold_show_menu.js
+++ client/plugin/touch_hold_show_menu.js
@@ -10,14 +10,23 @@
 			EDITOR.addEvent("mouseClick", {fun: ltTouchDown, dir: "down", targetClass:"fileCanvas", button: 0});
 			EDITOR.addEvent("mouseClick", {fun: ltTouchUp, dir: "up", targetClass:"fileCanvas", button: 0});
 			
+			EDITOR.addEvent("mouseMove", {fun: ltMouseMove, targetClass:"fileCanvas"});
+			
 		},
 		unload: function unloadLongTouchShowMenu() {
 			
 			EDITOR.removeEvent("mouseClick", ltTouchDown);
 			EDITOR.removeEvent("mouseClick", ltTouchUp);
 			
+			EDITOR.removeEvent("mouseMove", ltMouseMove);
+			
 		}
 	});
+	
+	function ltMouseMove(mouseX, mouseY, target, mouseMoveEvent) {
+		// Don't show the menu when swiping
+		if(mouseMoveEvent.type=="touchmove") clearTimeout(touchDownTimer);
+		}
 	
 	function ltTouchDown(mouseX, mouseY, caret, direction, button, target, keyboardCombo, mouseDownEvent) {
 		
