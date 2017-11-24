11b5334a9d83a7953435b52b7e06f1e2e9702ad9
Z <zeta@zetafiles.org>
2017-11-24 10:57:24 +0100

    Unload touch hold show meny plugin if the user has a real mouse

--- client/plugin/touch_hold_show_menu.js
+++ client/plugin/touch_hold_show_menu.js
@@ -7,21 +7,23 @@
 		desc: "Show (context) menu on long touch",
 		load: function loadLongTouchShowMenu() {
 			
-			EDITOR.addEvent("mouseClick", {fun: ltTouchDown, dir: "down", targetClass:"fileCanvas", button: 0});
-			EDITOR.addEvent("mouseClick", {fun: ltTouchUp, dir: "up", targetClass:"fileCanvas", button: 0});
+			EDITOR.addEvent("mouseClick", {fun: ltTouchDown, dir: "down", targetClass:"fileCanvas"});
+			EDITOR.addEvent("mouseClick", {fun: ltTouchUp, dir: "up", targetClass:"fileCanvas"});
 			
 			EDITOR.addEvent("mouseMove", {fun: ltMouseMove, targetClass:"fileCanvas"});
 			
 		},
-		unload: function unloadLongTouchShowMenu() {
-			
-			EDITOR.removeEvent("mouseClick", ltTouchDown);
-			EDITOR.removeEvent("mouseClick", ltTouchUp);
-			
-			EDITOR.removeEvent("mouseMove", ltMouseMove);
-			
-		}
+		unload: unloadLongTouchShowMenu
 	});
+	
+	function unloadLongTouchShowMenu() {
+		
+		EDITOR.removeEvent("mouseClick", ltTouchDown);
+		EDITOR.removeEvent("mouseClick", ltTouchUp);
+		
+		EDITOR.removeEvent("mouseMove", ltMouseMove);
+		
+	}
 	
 	function ltMouseMove(mouseX, mouseY, target, mouseMoveEvent) {
 		// Don't show the menu when swiping
@@ -30,10 +32,17 @@
 	
 	function ltTouchDown(mouseX, mouseY, caret, direction, button, target, keyboardCombo, mouseDownEvent) {
 		
-		if(mouseDownEvent.type != "touchstart") return;
+		if(mouseDownEvent.which > 1) {
+			// The user is using a regular mouse, so this plugin is not necessary
+			// Unload the module to free up resources
+			unloadLongTouchShowMenu();
+			return true;
+		}
 		
-		if(mouseX > (EDITOR.view.canvasWidth - EDITOR.settings.scrollZone)) return;
-		if(mouseY > (EDITOR.view.canvasHeight - EDITOR.settings.scrollZone)) return;
+		if(mouseDownEvent.type != "touchstart") return true;
+		
+		if(mouseX > (EDITOR.view.canvasWidth - EDITOR.settings.scrollZone)) return true;
+		if(mouseY > (EDITOR.view.canvasHeight - EDITOR.settings.scrollZone)) return true;
 		
 		console.log(mouseDownEvent);
 		
