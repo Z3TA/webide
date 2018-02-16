44dd21a5987a3bcc30f47598b350bd8a1819e888
Z <zeta@zetafiles.org>
2018-02-16 09:23:31 +0100

    keep in repo so I don't lose it

---
+++ webide_release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /home/Z/Projects/jzedit/
+./release.sh
+
+rsync -r --delete temp/release/server/ root@ben.100m.se:/srv/jzedit/
+rsync -r --delete node_modules/ root@ben.100m.se:/srv/jzedit/node_modules/
+rsync -r --delete client/noVNC/ root@ben.100m.se:/srv/jzedit/client/noVNC/
+
+ssh root@ben.100m.se /bin/bash << EOF
+cd /srv/jzedit/
+nodejs update.js
+
+EOF
+
+# rsync -rv
