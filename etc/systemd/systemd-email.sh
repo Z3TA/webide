03fc0bd199c971d875ab9f2efc6ea858360607be
Z <zeta@zetafiles.org>
2018-03-08 09:41:48 +0100

    forgot to add the service reporting systemd service files

---
+++ etc/systemd/systemd-email.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Send error report
+#
+
+/usr/bin/sendmail -t <<ERRMAIL
+To: $1
+From: systemd <root@$HOSTNAME>
+Subject: $2
+Content-Transfer-Encoding: 8bit
+Content-Type: text/plain; charset=UTF-8
+
+$(systemctl status --full "$2")
+ERRMAIL
