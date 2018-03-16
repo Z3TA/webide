95c15ea9b8d200cc5dee9acb9e48e3fa7a12181d
Z <zeta@zetafiles.org>
2018-03-16 08:17:02 +0100

    "don't want to save pw in bash history"

--- hashPw.js
+++ hashPw.js
@@ -15,14 +15,64 @@ var pwHash = require("./server/pwHash.js");
 	
 	var pw = process.argv[2]
 	
-	if(!pw) {
-	console.warn("Write the password to be hashed");
-	process.exit();
-	}
+if(pw) hash(pw);
+else getPassword(hash);
+
+
+function getPassword(callback) {
 	
-	//console.log(pw);
+	var BACKSPACE = String.fromCharCode(127);
+	
+	var prompt = "Password: ";
+	
+	process.stdout.write(prompt);
+	
+	var stdin = process.stdin;
+	stdin.resume();
+	stdin.setRawMode(true);
+	stdin.resume();
+	stdin.setEncoding('utf8');
+	
+	var password = '';
+	stdin.on('data', function (ch) {
+		ch = ch.toString('utf8');
+		
+		switch (ch) {
+			case "\n":
+			case "\r":
+			case "\u0004":
+			// They've finished typing their password
+			process.stdout.write('\n');
+			stdin.setRawMode(false);
+			stdin.pause();
+			callback(password);
+			break;
+			case "\u0003":
+			// Ctrl-C
+			process.exit();
+			break;
+			case BACKSPACE:
+			password = password.slice(0, password.length - 1);
+			process.stdout.clearLine();
+			process.stdout.cursorTo(0);
+			process.stdout.write(prompt);
+			process.stdout.write(password.split('').map(function () {
+				return '*';
+			}).join(''));
+			break;
+			default:
+			// More passsword characters
+			process.stdout.write('*');
+			password += ch;
+			break;
+		}
+	});
+}
+
+function hash(pw) {
+	console.log("hasing: " + pw);
 	
 console.time("Time taken");
 	console.log(pwHash(pw));
 console.timeEnd("Time taken");
-	
+}
