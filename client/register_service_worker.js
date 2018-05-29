ea63e68bdcd4b2bb733edc6b59984ca68f33383e
Z <zeta@zetafiles.org>
2018-05-29 07:29:41 +0200

    Maybe this will make the service worker update itself !?

--- client/register_service_worker.js
+++ client/register_service_worker.js
@@ -3,6 +3,7 @@ if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/serviceWorker.js', {scope: '/'}).then(function(reg) {
 // registration worked
 console.log('ServiceWorker Registration succeeded. Scope is ' + reg.scope);
+		reg.update();
 }).catch(function(error) {
 // registration failed
 		console.log('ServiceWorker Registration failed with ' + error);
