6525e0b522db01b2d9b797e1140303fb65ae2a4e
Z <zeta@zetafiles.org>
2018-08-20 14:48:01 +0200

    Open welcome right away if it's the first time using the editor. This will make it feel more snappier

--- client/plugin/openWelcome.js
+++ client/plugin/openWelcome.js
@@ -12,20 +12,29 @@
 	
 	function openWelcome(login) {
 		
+		if(EDITOR.startedCounter == 1) {
+			// First time the editor is started. Open the welcome right away
+			openWelcomeFile();
+		}
+		else {
+			
 		// The user might have edited the file, so it will be opened by the reopen_files plugin.
 		// So wait a bit ...
 		
 		setTimeout(function wait() {
 			if(Object.keys(EDITOR.files).length === 0 && EDITOR.openFileQueue.length == 0) {
-				
-				EDITOR.openFile("/wwwpub/welcome.html", undefined, function fileOpened(err, file) {
-					
-					if(err) console.warn(err);
-					
-				});
+					openWelcomeFile();
 				}
 		}, 1000); // reopen_files seem to take an awful long time
 	}
+	}
 	
+	function openWelcomeFile() {
+		EDITOR.openFile("/wwwpub/welcome.html", undefined, function fileOpened(err, file) {
+			
+			if(err) console.warn(err);
+			
+		});
+	}
 	
 })();
