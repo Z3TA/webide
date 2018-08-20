db302e6d2e9ac501d982dc9cc0df7a812b8e06c5
Z <zeta@zetafiles.org>
2018-08-20 15:07:43 +0200

    dont't open the welcome if there's something in the query string

--- client/plugin/openWelcome.js
+++ client/plugin/openWelcome.js
@@ -8,12 +8,19 @@
 	if(QUERY_STRING["embed"]) return;
 	
 	// The file is not available until the user has logged in
+	// (we could cheat and serve it with the editor itself)
+	
 	CLIENT.on("loginSuccess", openWelcome, 2000);
 	
 	function openWelcome(login) {
 		
+		if(window.location.search) {
+			console.log("Not opening welcome file because window.location.search=" + window.location.search);
+			return;
+		}
+		
 		if(EDITOR.startedCounter == 1) {
-			// First time the editor is started. Open the welcome right away
+			// First time the editor is started. And nothing in query string (eg no repos to download) Open the welcome right away
 			openWelcomeFile();
 		}
 		else {
