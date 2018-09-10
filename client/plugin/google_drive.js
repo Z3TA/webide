30e3fc8151b5e4897c1f3a82c00d3f65896fd22c
Z <zeta@zetafiles.org>
2018-09-10 15:26:52 +0200

    gcsf is currently just compiled for Linux. We want the editor to be as platform independed as possible and gcsf got a rather large binary size

--- client/plugin/google_drive.js
+++ client/plugin/google_drive.js
@@ -12,6 +12,9 @@ var menuItem;
 	var GD_MENU_NOT_CONNECTED = "Google Drive";
 	var GD_MENU_CONNECTED = "Google Drive (logout)";
 	
+	if(window.location.hostname == "127.0.0.1") return console.warn("Disabling Google Drive integration when running locally");
+	// Don't bother compiling gcsf for every platform (we might make it into a node module if Google Drive becomes a wanted feature)
+	
 EDITOR.plugin({
 	desc: "Mount Google Drive",
 	load: function() {
