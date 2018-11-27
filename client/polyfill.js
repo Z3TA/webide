ff8d2a2ccb98c995fca44b902ba5fb6421f6cd19
Z <zeta@zetafiles.org>
2018-11-27 15:25:57 +0100

    Can't put vibrate into a variable because it needs "this" to be navigator

--- client/polyfill.js
+++ client/polyfill.js
@@ -8,6 +8,8 @@ if(typeof console == "undefined") console = {};
 if(typeof console.log == "undefined") console.log = function() {};
 if(typeof console.error == "undefined") console.error = function() {};
 
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+
 	if (!Array.isArray) {
 	Array.isArray = function(arg) {
 	return Object.prototype.toString.call(arg) === '[object Array]';
