9f744cd3a1679a3e6ca9f8e86c9d9c2797d450aa
Z <zeta@zetafiles.org>
2019-01-09 16:22:43 +0100

    Need feedback!

--- client/plugin/openWelcome.js
+++ client/plugin/openWelcome.js
@@ -19,6 +19,12 @@
 			return;
 		}
 		
+		var host = window.location.hostname;
+		if(host == "127.0.0.1" || host.indexOf("192.168") != -1) {
+			console.log("Not opening welcome file because window.location.hostname=" + host);
+			return;
+		}
+		
 		if(EDITOR.startedCounter == 1) {
 			// First time the editor is started. And nothing in query string (eg no repos to download) Open the welcome right away
 			openWelcomeFile();
