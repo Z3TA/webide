c04cc20119e52ef2dc79c8436a55e086ff69f07d
Z <zeta@zetafiles.org>
2019-01-16 10:31:14 +0100

    Consolas on Windows

--- client/settings_overload.js
+++ client/settings_overload.js
@@ -13,72 +13,30 @@
 	//EDITOR.settings.style.xmlTagColor = "rgb(255,0,0)";
 	
 	var browser = UTIL.checkBrowser();
-	
-	console.log("RUNTIME=" + RUNTIME + " browser=" + browser + " process.platform=" + process.platform);
+	var ligatures = false;
 
-	if(RUNTIME=="browser") {
-	
-	// We better use a web safe font in the browser
-		EDITOR.settings.style.font = "Courier New, Courier, monospace";
-		EDITOR.settings.style.highlightMatchFont = "bold 15px Courier New, Courier, monospace";
-	EDITOR.settings.style.fontSize = 15;
-	EDITOR.settings.gridHeight = 23;
-	EDITOR.settings.gridWidth = 9;
-	
-		
-		var width = parseInt(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-		var height = parseInt(window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
-		
-		//alert("width=" + width + " height=" + height + "");
-		
-		
-		if( (!isNaN(width) && width <= 600) || (!isNaN(height) && height <= 250) ) {
-			// Use a smaller text size on small screens
-			// On some devices which doesn't have high DPI screen, the small text size is undreadable
-			/*
-				alert("Setting extra small text size due to width=" + width + " and height=" + height);
-				EDITOR.settings.style.font = "Courier New, Courier, monospace";
-				EDITOR.settings.style.highlightMatchFont = "bold 10px Courier New, Courier, monospace";
-				EDITOR.settings.style.fontSize = 10;
-				EDITOR.settings.gridHeight = 14;
-				EDITOR.settings.gridWidth = 6;
-				EDITOR.settings.leftMargin = 24;
-				EDITOR.settings.rightMargin = 24;
-				EDITOR.settings.topMargin = 4;
-				EDITOR.settings.bottomMargin = 2;
-			*/
-		}
-		else {
-			
-			// Web safe fonts are ugly, try to load a nice font ...
-			
-			if(browser != "Firefox") { // Firefox have wierd kerning/spacing
-				UTIL.loadCSS("gfx/font/DejaVuSansMono/DejaVuSansMono.css");
-				EDITOR.settings.style.font = "DejaVuSansMono";
-				EDITOR.settings.style.highlightMatchFont = "bold 13px DejaVuSansMono";
-				EDITOR.settings.style.fontSize = 13;
-				EDITOR.settings.gridHeight = 22;
-				EDITOR.settings.gridWidth = 7.83;
-			}
-		}
-		
+	console.log("RUNTIME=" + RUNTIME + " browser=" + browser + " process.platform=" + process.platform + " ligatures=" + false);
+
+	if(ligatures && (browser == "Chrome" || browser == "Safari" || browser == "Firefox")) {
 		/*
 			Ligatures
 			---------
-		Ligatures are basically two letter as one. Fira code for example makes => into an arrow.
-		
-		if(a != b && b <= c && x != y)
-		
-		<* <*> <+> <$> *** <| |> <|> !! || === ==> <<< >>> <> +++ <- -> => >> << >>= =<< .. ... :: -< >- -<< >>- ++ /= ==
-		
-	*/
-		if(1==2 && (browser == "Chrome" || browser == "Safari" || browser == "Firefox")) {
-			// Ligatures are confirmed to work in: Chrome/Chromium/Opera, Firefox, Safari, but not IE
-			// Download Fira Code (https://github.com/tonsky/FiraCode/releases/) and put it in gfx/font/ and change the path below:
+			Ligatures are basically two letter as one. Fira code for example makes => into an arrow.
+			
+			if(a != b && b <= c && x != y)
+			
+			<* <*> <+> <$> *** <| |> <|> !! || === ==> <<< >>> <> +++ <- -> => >> << >>= =<< .. ... :: -< >- -<< >>- ++ /= ==
+			
+			Ligatures are confirmed to work in: Chrome/Chromium/Opera, Firefox, Safari, but not IE
+			Download Fira Code (https://github.com/tonsky/FiraCode/releases/) and put it in gfx/font/ 
+			and change the path (/FiraCode_1.204/) below:
+			
+			*/
 		UTIL.loadCSS("gfx/font/FiraCode_1.204/fira_code.css");
 		EDITOR.settings.style.font = "Fira Code";
-		if(UTIL.checkBrowser() == "Firefox") {
-				// Firefox renders font's differently
+		
+		if(browser == "Firefox") {
+			// Firefox renders font's differently
 			EDITOR.settings.style.fontSize = 15;
 			EDITOR.settings.gridHeight = 23;
 			EDITOR.settings.gridWidth = 9;
@@ -90,14 +48,10 @@
 			EDITOR.settings.gridWidth = 7.83;
 			EDITOR.settings.style.highlightMatchFont = "bold 14px Fira Code";
 		}
-		}
-		
-		
-		
 	}
-
-	if(process.platform == "win32" && (RUNTIME == "nw.js" || browser == "Chrome") && EDITOR.settings.sub_pixel_antialias == true) {
+	else if(process.platform == "win32" && (RUNTIME == "nw.js" || browser == "Chrome") && window.devicePixelRatio == 1) {
 		// Only Chrome/Chromium/nw.js supports sub-pixel antialias. Consolas needs sub-pixel antialias to look good
+		EDITOR.settings.sub_pixel_antialias == true;
 		
 	/*
 			When Windows with "Smooth edges of screen fonts" turned off, these font's look good:
@@ -121,8 +75,7 @@
 	EDITOR.settings.gridWidth = 8.25;
 	
 }
-
-	if(process.platform == "linux" && RUNTIME == "nw.js") {
+else if(process.platform == "linux" && RUNTIME == "nw.js") {
 	
 	/*
 			Linux does not have Consolas (see README.txt on how to download it if you are desperate)
@@ -161,5 +114,50 @@
 	EDITOR.settings.gridWidth = 7.83;
 	
 }
-	
+	else if(RUNTIME=="browser" && browser != "Firefox") {
+		// Firefox have font render issues
+		// Web safe fonts are ugly, try to load a nice font ...
+		if(browser != "Firefox") { // Firefox have wierd kerning/spacing
+			UTIL.loadCSS("gfx/font/DejaVuSansMono/DejaVuSansMono.css");
+			EDITOR.settings.style.font = "DejaVuSansMono";
+			EDITOR.settings.style.highlightMatchFont = "bold 13px DejaVuSansMono";
+			EDITOR.settings.style.fontSize = 13;
+			EDITOR.settings.gridHeight = 22;
+			EDITOR.settings.gridWidth = 7.83;
+		}
+	}
+	else if(RUNTIME=="browser") {
+		
+		// We better use a web safe font in the browser
+		EDITOR.settings.style.font = "Courier New, Courier, monospace";
+		EDITOR.settings.style.highlightMatchFont = "bold 15px Courier New, Courier, monospace";
+		EDITOR.settings.style.fontSize = 15;
+		EDITOR.settings.gridHeight = 23;
+		EDITOR.settings.gridWidth = 9;
+		
+		
+		var width = parseInt(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+		var height = parseInt(window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+		
+		//alert("width=" + width + " height=" + height + "");
+		
+		
+		if( (!isNaN(width) && width <= 600) || (!isNaN(height) && height <= 250) ) {
+			// Use a smaller text size on small screens
+			// On some devices which doesn't have high DPI screen, the small text size is undreadable
+			/*
+				alert("Setting extra small text size due to width=" + width + " and height=" + height);
+				EDITOR.settings.style.font = "Courier New, Courier, monospace";
+				EDITOR.settings.style.highlightMatchFont = "bold 10px Courier New, Courier, monospace";
+				EDITOR.settings.style.fontSize = 10;
+				EDITOR.settings.gridHeight = 14;
+				EDITOR.settings.gridWidth = 6;
+				EDITOR.settings.leftMargin = 24;
+				EDITOR.settings.rightMargin = 24;
+				EDITOR.settings.topMargin = 4;
+				EDITOR.settings.bottomMargin = 2;
+			*/
+		}
+		
+	}
 })();
