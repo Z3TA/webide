e551ba8d3ce99256e7ea0601c7d5bcfd8e52a743
Z <zeta@zetafiles.org>
2019-06-10 14:14:37 +0200

    "cleanup"

--- release.sh
+++ release.sh
@@ -100,10 +100,13 @@ rm temp/release/linux/.hgignore
 rm temp/release/linux/getCommitId.js
 rm temp/release/linux/jz.xcf
 rm temp/release/linux/makebundle.js
+rm temp/release/linux/makebundle2.js
 rm temp/release/linux/changeset.js
 rm temp/release/linux/semver.js
 #rm temp/release/linux/SEMVER
 rm temp/release/linux/client/gfx/icon/test.htm
+rm temp/release/linux/replaceStringInFileWithContentOfFile.js
+
 
 echo "Removing unused fonts"
 find temp/release/linux/client/gfx/font/ ! -name 'liberationMono.css' ! -name 'LiberationMono-Regular.ttf' ! -name 'LiberationMono-Bold.ttf' ! -name 'DejaVuSansMono.css' ! -name 'DejaVuSansMono.ttf' ! -name 'DejaVuSansMono-Bold.ttf' -type f -exec rm -f {} +
