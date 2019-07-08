580d2df98043368d88302903cd3d48932e8ced2c
Z <zeta@zetafiles.org>
2019-07-08 10:57:09 +0200

    "Able to hide window menu"

---
+++ client/plugin/hide_window_menu.js
@@ -0,0 +1,34 @@
+
+(function() {
+
+var winMenuHide;
+	var ctxWinMenuShow;
+	
+	EDITOR.plugin({
+		desc: "Hide the window menu",
+		load: function loadHideWindowMenu() {
+
+			winMenuHide = EDITOR.windowMenu.add("Window menu", ["View", 13], hideWindowMenu);
+winMenuHide.activate();
+
+},
+		unload: function unloadHideWindowMenu() {
+
+			if(ctxWinMenuShow) EDITOR.ctxMenu.remove(ctxWinMenuShow);
+			
+		}
+});
+	
+	function hideWindowMenu() {
+		EDITOR.windowMenu.disable();
+		
+		ctxWinMenuShow = EDITOR.ctxMenu.add("Show window menu", showWindowMenu, 20);
+	}
+	
+	function showWindowMenu() {
+		EDITOR.windowMenu.enable();
+		EDITOR.ctxMenu.remove(ctxWinMenuShow);
+		ctxWinMenuShow = undefined;
+	}
+
+})();
