09638fb9354f0a00ce993d2e2acf2f6340d19240
Z <zeta@zetafiles.org>
2019-08-07 16:37:05 +0200

    "Expected to find it in the View menu"

--- client/plugin/text-mode-indentation.js
+++ client/plugin/text-mode-indentation.js
@@ -36,7 +36,7 @@
 			
 			EDITOR.on("showMenu", showWhiteSpaceMaybe);
 			
-			winMenuShowIndentation = EDITOR.windowMenu.add("Show White-space", ["Edit", 12], toggleShowWhiteSpace);
+			winMenuShowIndentation = EDITOR.windowMenu.add("Show White-space", ["View", 12], toggleShowWhiteSpace);
 			
 			if(QUERY_STRING["whiteSpace"]) toggleShowWhiteSpace();
 			
