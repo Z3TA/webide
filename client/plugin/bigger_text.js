f94aa57cffb2cf9e2fad8abb982da1e445c71874
Z <zeta@zetafiles.org>
2019-08-20 11:34:13 +0200

    "Text was too small when using smartphone as screen"

---
+++ client/plugin/bigger_text.js
@@ -0,0 +1,68 @@
+
+(function() {
+
+var winMenuBiggerText;
+	var bigTextActive = false;
+	var originalFontSize, originalGridHeight, originalGridWidth;
+	
+EDITOR.plugin({
+desc: "Increase text size",
+load: function loadBiggerText() {
+
+			winMenuBiggerText = EDITOR.windowMenu.add("Big text", ["View", 15], toggleBigText);
+			
+			originalFontSize = EDITOR.settings.style.fontSize;
+			originalGridHeight = EDITOR.settings.gridHeight;
+			originalGridWidth = EDITOR.settings.gridWidth;
+			
+		},
+unload: function unloadBiggerText() {
+			EDITOR.windowMenu.remove(winMenuBiggerText);
+}
+});
+
+	function toggleBigText() {
+		
+		var matchHighlightFontSize = EDITOR.settings.style.highlightMatchFont.match(/(\d*)px/);
+		if(matchHighlightFontSize) {
+			var pxStr = matchHighlightFontSize[0];
+			var size = parseFloat(matchHighlightFontSize[1]);
+		}
+		
+		
+		if(bigTextActive) {
+			
+			EDITOR.settings.style.fontSize = EDITOR.settings.style.fontSize / 2;
+			EDITOR.settings.gridHeight = EDITOR.settings.gridHeight / 2;
+			EDITOR.settings.gridWidth = EDITOR.settings.gridWidth / 2;
+			
+			if(matchHighlightFontSize) {
+				EDITOR.settings.style.highlightMatchFont = EDITOR.settings.style.highlightMatchFont.replace(pxStr, size*2);
+			}
+			
+			bigTextActive = false;
+			
+			winMenuBiggerText.deactivate();
+		}
+		else {
+			var fontSize = parseFloat(EDITOR.settings.style.fontSize);
+			
+			EDITOR.settings.style.fontSize = EDITOR.settings.style.fontSize * 2;
+			EDITOR.settings.gridHeight = EDITOR.settings.gridHeight * 2;
+			EDITOR.settings.gridWidth = EDITOR.settings.gridWidth * 2;
+			
+			if(matchHighlightFontSize) {
+				EDITOR.settings.style.highlightMatchFont = EDITOR.settings.style.highlightMatchFont.replace(pxStr, size/2);
+			}
+			
+			bigTextActive = true;
+			
+			winMenuBiggerText.activate();
+		}
+		
+		
+		
+		EDITOR.resize(true);
+	}
+	
+})();
