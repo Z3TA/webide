f137a900422e5b470c1d370596d2e8e7b2c8950a
Z <zeta@zetafiles.org>
2019-08-22 22:37:26 +0200

    "It should be nice to be able to unregister the service worker if it's causing issues"

--- client/register_service_worker.js
+++ client/register_service_worker.js
@@ -1,4 +1,11 @@
-
+(function() {
+	
+	EDITOR.on("start", registerServiceWorker);
+	
+	function registerServiceWorker() {
+		
+		console.log("Hello from register_service_worker.js");
+		
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/serviceWorker.js', {scope: '/'}).then(function(reg) {
 // registration worked
@@ -9,4 +16,23 @@ console.log('ServiceWorker Registration succeeded. Scope is ' + reg.scope);
 // registration failed
 		console.log('ServiceWorker Registration failed with ' + error);
 });
-}
+
+			var windowMenuUnregisterServiceWorker = EDITOR.windowMenu.add("Unregister service worker", ["Editor", 90], unregisterServiceWorker);
+		}
+		console.warn("Service worker not supported by " + (typeof BROWSER != "undefined" ? BROWSER : "browser"))
+		
+	}
+	
+	function unregisterServiceWorker() {
+		navigator.serviceWorker.getRegistrations().then(function(registrations) {
+			
+			for(var registration in registrations) {
+				registrations[registration].unregister()
+			}
+			
+		}).catch(function(err) {
+			alertBox("Failed to get registerd service workers: " + err.message)
+		});
+	}
+	
+})();
