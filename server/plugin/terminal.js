17a6221338edad4d3b7688e49fc47e1712fa6ebc
Z <zeta@zetafiles.org>
2019-10-23 15:31:49 +0200

    "TerminaL chdir(2) failed.: No such file or directory Because an unsaved file was open, which path did not yet exist"

--- server/plugin/terminal.js
+++ server/plugin/terminal.js
@@ -17,6 +17,9 @@ catch(err) {
 	ptyMissing = err.message;
 }
 
+var UTIL = require("../../client/UTIL.js");
+var module_fs = require("fs");
+
 var TERMINAL_COUNTER = 0;
 
 var TERMINAL = {};
@@ -42,50 +45,70 @@ var defaultEnv = {
 
 function newTerminal(user, json, callback) {
 	
+	var defaultCwd = process.env.HOME;
+	
 	var cols = json.cols || 80;
 	var rows = json.rows || 30;
-	var cwd = json.cwd || process.env.HOME;
+	var cwd = json.cwd || defaultCwd;
 	var env = json.env || process.env || defaultEnv;
 	var defaultShell = module_os.platform() == 'win32' ? 'powershell.exe' : '/bin/bash';
 	var exec = json.exec || defaultShell;
 	var termId = json.id || ++TERMINAL_COUNTER;
 	
-	console.log("env=" + JSON.stringify(env));
+	console.log("cwd=" + cwd + " env=" + JSON.stringify(env) + "");
 	
-	if(termId < TERMINAL_COUNTER) {
-		var error = new Error("Terminal id needs to be " + TERMINAL_COUNTER + " or higher");
-		error.code = "TERMINAL_ID_COUNTER";
-		return callback(error);
+	if(cwd != defaultCwd) {
+		// Check if the dir exist first
+		var folders = UTIL.getFolders(cwd);
+		checkDirectory(folders.pop());
+	}
+	else open(cwd);
+	
+	function checkDirectory(testFolder) {
+		module_fs.stat(testFolder, function(err, stats) {
+			if(!err && stats.isDirectory()) open(testFolder);
+			else if(folders.length == 0) open(defaultCwd);
+			else checkDirectory(folders.pop());
+		});
 	}
 	
-	if(termId > TERMINAL_COUNTER) TERMINAL_COUNTER = termId;
 	
-	TERMINALS[termId] = module_pty.spawn(exec, [], {
-		name: 'xterm-color',
-		cols: cols,
-		rows: rows,
-		cwd: cwd,
-		env: env
-	});
-	
+	function open(cwd) {
+		if(termId < TERMINAL_COUNTER) {
+			var error = new Error("Terminal id needs to be " + TERMINAL_COUNTER + " or higher");
+			error.code = "TERMINAL_ID_COUNTER";
+			return callback(error);
+		}
+		
+		if(termId > TERMINAL_COUNTER) TERMINAL_COUNTER = termId;
+		
+		TERMINALS[termId] = module_pty.spawn(exec, [], {
+			name: 'xterm-color',
+			cols: cols,
+			rows: rows,
+			cwd: cwd,
+			env: env
+		});
+		
 		TERMINALS[termId].on('data', function(data) {
 			user.send({terminal: {id: termId, data: data}});
 		});
 		
-	TERMINALS[termId].on('exit', function(code, signal) {
-		user.send({terminal: {
-				id: termId, 
-				exit: {
-		code: code, 
-		signal: signal
-		}
-		}
+		TERMINALS[termId].on('exit', function(code, signal) {
+			user.send({terminal: {
+					id: termId, 
+					exit: {
+						code: code, 
+						signal: signal
+					}
+				}
 		});
 		delete TERMINALS[termId];
 	});
 	
 	callback(null, {id: termId, exec: TERMINALS[termId].process});
 	}
+}
 
 function terminalWrite(user, json, callback) {
 	
