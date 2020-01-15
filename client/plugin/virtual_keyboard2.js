88fd439729896c40ea8b08fc90eb448730da200d
Z <zeta@zetafiles.org>
2020-01-15 16:07:11 +0100

    "got TypeError: element.tagName is undefined"

--- client/plugin/virtual_keyboard2.js
+++ client/plugin/virtual_keyboard2.js
@@ -222,13 +222,17 @@
 	
 	function hideVirtualKeyboardIfTextElementIsFocused(el) {
 		if(el == undefined) el = document.activeElement;
+		
 		if(isTextBox(el)) hideBuiltinKeyboard();
 	}
 	
 	function isTextBox(element) {
+		if(!el.tagName) return false;
+		
 		var tagName = element.tagName.toLowerCase();
 		if (tagName === 'textarea') return true;
 		if (tagName !== 'input') return false;
+
 		var type = element.getAttribute('type').toLowerCase(),
 		// if any of these input types is not supported by a browser, it will behave as input type text.
 		inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
