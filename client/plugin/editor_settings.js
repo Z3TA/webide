79f3e6fa2f95ba2cf89e69551f43ad208cadd24d
Z <zeta@zetafiles.org>
2020-02-19 16:03:25 +0100

    "Able to enable JSX"

---
+++ client/plugin/editor_settings.js
@@ -0,0 +1,61 @@
+
+/*
+	
+	Update editor settings
+	
+*/
+
+(function() {
+	"use strict";
+	
+	var windowMenu;
+	
+	var wmJsx;
+	
+	EDITOR.plugin({
+		desc: "Update editor settings from window menu",
+		load: function loadEditorSettings() {
+			wmJsx = EDITOR.windowMenu.add(S("parse jsx"), [S("editor"), S("settings"), 50], toggleJsx);
+			
+			EDITOR.loadSettings("jsx", EDITOR.settings.jsx, function connectionStatusSettingsLoaded(state) {
+				if(state != EDITOR.settings.jsx) toggleJsx(state);
+			});
+			
+			// Always parse JSX (can be buggy) by default on .jsx and .tsx files
+			
+		},
+		unload: function unloadEditorSettings() {
+			EDITOR.windowMenu.remove(wmJsx);
+			wmJsx = null;
+			
+			
+		}
+	});
+	
+	
+	function toggleJsx(toState) {
+		
+		if(toState == undefined) {
+			var state = EDITOR.settings.jsx;
+		}
+		else {
+			var state = !toState;
+		}
+		
+		if(state === true) {
+			EDITOR.settings.jsx = false;
+			wmJsx.deactivate();
+		}
+		else {
+			EDITOR.settings.jsx = true;
+			wmJsx.activate();
+		}
+		
+		EDITOR.saveSettings("jsx", EDITOR.settings.jsx);
+		
+		if(toState == undefined) alertBox("You need to reload files for settings to take effect");
+		
+	}
+	
+	
+})();
