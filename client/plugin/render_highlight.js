df10c5b3564bf8a39b41876b29f531ea9914c98f
Z <zeta@zetafiles.org>
2020-04-14 13:49:37 +0200

    "use gridWalker"

--- client/plugin/render_highlight.js
+++ client/plugin/render_highlight.js
@@ -37,69 +37,30 @@
 
 		ctx.fillStyle=EDITOR.settings.style.highlightTextBg;
 	
-
-
-		
-		for(var row = 0; row < buffer.length; row++) {
-			
-			
-			
+		var startLeft = 0;
+		for(var row = 0, walker; row < buffer.length; row++) {
 			top = EDITOR.settings.topMargin + (row + startRow) * EDITOR.settings.gridHeight;
-			
-			/*
-			if(buffer[row].selected) { // The whole row is selected
-				ctx.rect(EDITOR.settings.leftMargin, top,	EDITOR.settings.gridWidth * buffer[row].length, EDITOR.settings.gridHeight);
-			}
-			*/
-			
 			indentation = buffer[row].indentation;
-			
-			tabIndention = 0;
-			while(tabIndention < buffer[row].length && buffer[row][tabIndention].char == "\t") tabIndention++;
-			indentation += tabIndention;
-			
-			left = EDITOR.settings.leftMargin + (indentation * EDITOR.settings.tabSpace - file.startColumn) * EDITOR.settings.gridWidth;
-			
-			var tabColumnTextLengthAdjustment = 0;
-			for(var col = 0; col < buffer[row].length; col++) {
-				charWidth = EDITOR.glyphWidth(file, buffer[row][col].index);
-				
-				if( col >= tabIndention && buffer[row][col].char == "\t") {
-					charWidth = 8 - (col-tabIndention+tabColumnTextLengthAdjustment) % 8;;
+			walker = EDITOR.gridWalker(buffer[row]);
+			startLeft = EDITOR.settings.leftMargin + (indentation * EDITOR.settings.tabSpace - file.startColumn) * EDITOR.settings.gridWidth;
+			while(!walker.done) {
+				walker.next();
+				if(buffer[row][walker.col].highlighted && !buffer[row][walker.col].selected) {
+					ctx.rect(
+					startLeft + (walker.totalWidth - walker.charWidth) * EDITOR.settings.gridWidth,
+					top,
+					walker.charWidth * EDITOR.settings.gridWidth,
+					EDITOR.settings.gridHeight
+					);
 				}
-				else if( UTIL.isSurrogateStart(buffer[row][col].char) ) {
-					
-					
-					if( buffer[row][col+2] && UTIL.isSurrogateModifierStart(buffer[row][col+2].char) ) {
-						col+= 3;
-						tabColumnTextLengthAdjustment -= 3;
-					}
-					else if( buffer[row][col+1] ) {
-						col++;
-						tabColumnTextLengthAdjustment -= 1;
-					}
-				}
-				
-				//if(isNaN(left)) throw new Error("left is NaN");
-				//if(isNaN(top)) throw new Error("top is NaN");
-				
-				if(buffer[row][col] && buffer[row][col].highlighted && !buffer[row][col].selected) {
-					ctx.rect(left, top,EDITOR.settings.gridWidth*charWidth, EDITOR.settings.gridHeight);
-				}
-				
-				left += EDITOR.settings.gridWidth*charWidth;
-				
-				if(charWidth > 1) tabColumnTextLengthAdjustment += charWidth-1;
 			}
-			
-			
 		}
 		
 		ctx.fill();
-
-		//ctx.stroke();
+		
+		return;
+		
 		//console.timeEnd("highlightRender");
-
 
 	}
 
