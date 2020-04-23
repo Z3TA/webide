93dca44935e160719cd18b7c48b4299cf1b19aef
Z <zeta@zetafiles.org>
2020-04-23 13:44:23 +0200

    "close without saving on Mac"

--- client/plugin/file_tabs.js
+++ client/plugin/file_tabs.js
@@ -613,6 +613,9 @@ tabFileText.setAttribute("path", path);
 		
 		function contextmenu(ev) {
 			console.log("file_tabs: Showing context menu... ev.target=", ev.target);
+			
+			if(ev.target.className=="closeFileTab") return true; // Ctrl-clicking closes the file without asking to save
+			
 			EDITOR.ctxMenu.show(ev || event);
 			EDITOR.ctxMenu.addTemp("Close all files except this one", true, closeAllOtherFiles);
 			EDITOR.ctxMenu.addTemp("Reopen last closed file", true, reopenFileTab);
