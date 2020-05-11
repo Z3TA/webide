c487d51fe194f4be9334e21fd3a0ec5eeaa6cf34
Z <zeta@zetafiles.org>
2020-05-11 09:46:01 +0200

    "TypeError: null is not an object (evaluating 'e.getAttribute(\"type\").toLowerCase')"

--- client/plugin/virtual_keyboard2.js
+++ client/plugin/virtual_keyboard2.js
@@ -227,15 +227,17 @@
 	}
 	
 	function isTextBox(element) {
+		if(typeof element != "object") return false;
 		if(!element.tagName) return false;
 		
 		var tagName = element.tagName.toLowerCase();
 		if (tagName === 'textarea') return true;
 		if (tagName !== 'input') return false;
 
-		var type = element.getAttribute('type').toLowerCase(),
+		var type = element.getAttribute('type');
+		var type = (typeof type == "string") && type.toLowerCase();
 		// if any of these input types is not supported by a browser, it will behave as input type text.
-		inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
+		var inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
 		return inputTypes.indexOf(type) >= 0;
 	}
 	
