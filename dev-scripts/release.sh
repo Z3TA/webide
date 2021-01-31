d87560d722f0f06d7d58d80b2d6a4eae5dda2cab
Z <zeta@zetafiles.org>
2021-01-31 22:49:29 +0100

    "it's now a bash script"

--- dev-scripts/release.sh
+++ dev-scripts/release.sh
@@ -153,7 +153,7 @@ rm -rf temp/release/linux/dockervm/
 
 rm temp/release/linux/gcsf
 rm temp/release/linux/adduser.js
-rm temp/release/linux/cloudide_install.js
+rm temp/release/linux/cloudide_install.sh
 rm temp/release/linux/nodejs_init.js
 rm temp/release/linux/nodejs_init_worker.js
 rm temp/release/linux/removeuser.js
