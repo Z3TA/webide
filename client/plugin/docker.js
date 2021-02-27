6c7cdf943a4082082fdb4cd8b91994301f6c0ecc
Z <zeta@zetafiles.org>
2021-02-27 23:44:15 +0100

    "dockerDaemon has already timed out! Consider increasing the timeout=30000 Request took 118 seconds... Hopefully the user don't want to run Docker within the first two minutes..."

--- client/plugin/docker.js
+++ client/plugin/docker.js
@@ -60,7 +60,7 @@ windowMenu.hide();
 return;
 		}
 		
-		CLIENT.cmd("dockerDaemon", {command: "status"}, 90000, function dockerStatus(err, status) {
+		CLIENT.cmd("dockerDaemon", {command: "status"}, 120000, function dockerStatus(err, status) {
 			if(err) {
 				if(err.code == "ENOSUPPORT") {
 					console.warn("Disabling Docker plugin");
