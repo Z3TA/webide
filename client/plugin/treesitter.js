fed2fb0179129e53ef73f725d2bf88c206e9ee6f
Z <zeta@zetafiles.org>
2021-03-01 00:02:23 +0100

    "trying to learn treesitter"

--- client/plugin/treesitter.js
+++ client/plugin/treesitter.js
@@ -9,6 +9,10 @@
 	npm install --save-dev tree-sitter-cli tree-sitter-javascript
 	npx tree-sitter build-wasm node_modules/tree-sitter-javascript
 
+	If it doesn't work, wget the files from https://tree-sitter.github.io/tree-sitter/playground
+
+	Gotach: Content-Security-Policy prevents running wasm in Chrome !?
+
 */
 (function() {
 	"use strict";
@@ -95,18 +99,76 @@
 
 		var tree = parsers[file.path].parse(file.text);
 
-		console.log("tree-sitter:treeSitterFileOpen: tree=" + JSON.stringify(tree, null, 2) );
+		console.log("tree-sitter: Parser=", parsers[file.path]);
+		console.log("tree-sitter: Parser.parse=", parsers[file.path].parse);
 
-		console.log("tree-sitter:treeSitterFileOpen: tree.rootNode=" + JSON.stringify(tree.rootNode, null, 2) );
+		console.log( "tree-sitter: parsers[file.path].language=", parsers[file.path].Language);
 
+		var testQuery = "(function_declaration name: (identifier) @fn-def)   (call_expression function: (identifier) @fn-ref) ";
+
+		var query = parsers[file.path].language.query(testQuery);
+		console.log( "tree-sitter: query=", query);
+
+		var matches = query.matches(tree.rootNode);
+		console.log( "tree-sitter: matches=", matches);
+
+		matches.forEach(function(match) {
+			console.log( "tree-sitter: match.captures[0].node.text=", match.captures[0].node.text );
+		});
+
+		
+
+		
+
+		/*
+
+
+
+		*/
+
+		// tree annd tree.rootNode is the same and only prints types and fields
+		//console.log("tree-sitter:treeSitterFileOpen: tree=" + JSON.stringify(tree, null, 2) );
+		//console.log("tree-sitter:treeSitterFileOpen: tree.rootNode=" + JSON.stringify(tree.rootNode, null, 2) );
 		console.log("tree-sitter:treeSitterFileOpen: tree.rootNode=", tree.rootNode );
+		//console.log("tree-sitter:treeSitterFileOpen: tree.rootNode=" + tree.rootNode.toString() );
 
-		console.log("tree-sitter:treeSitterFileOpen: tree.rootNode=" + tree.rootNode.toString() );
+		// tree and tree.rootNode seem to be the same
+		//console.log("tree-sitter:treeSitterFileOpen: tree.walk()=",  tree.walk() );
+
+		//console.log("tree-sitter:treeSitterFileOpen: tree.rootNode.walk()=", tree.rootNode.walk() );
 
 
-		console.log("tree-sitter:treeSitterFileOpen: tree.walk()=",  tree.walk() );
+		// Can only call child on tree.rootNode!
+		//console.log("tree-sitter:treeSitterFileOpen: tree.child(1)=", tree.child(1));
+		
+		//console.log( "tree-sitter: Object.getOwnPropertyNames(tree)=", Object.getOwnPropertyNames(tree) );
+		//console.log( "tree-sitter: Object.getOwnPropertyNames(tree.rootNode)=", Object.getOwnPropertyNames(tree.rootNode) );
 
-		console.log("tree-sitter:treeSitterFileOpen: tree.rootNode.walk()=", tree.rootNode.walk() );
+		
+		console.log("tree-sitter:tree.rootNode.toString()", tree.rootNode.toString());
+
+		//var cursor = tree.rootNode.walk();
+		//console.log("tree-sitter: cursor=", cursor);
+		//console.log("tree-sitter: cursor.prototype=", cursor.prototype);
+
+		//console.log("tree-sitter: cursor.tree.walk()=", cursor.tree.walk());
+
+		//console.log("tree-sitter: cursor.gotoParent()=", cursor.gotoParent() ); // returns false
+
+		//console.log( "tree-sitter: cursor.nodeText=", cursor.nodeText );
+
+		//console.log( "tree-sitter: cursor.gotoFirstChild()=", cursor.gotoFirstChild() ); // returns true
+		//console.log( "tree-sitter: cursor.gotoNextSibling()=", cursor.gotoNextSibling() ); // returns true
+
+		// cursors not fully implemented in JavaScript bindings !?
+
+		console.log( "tree-sitter: tree.rootNode=", tree.rootNode );
+		console.log( "tree-sitter: tree.rootNode.childCount=", tree.rootNode.childCount) ;
+		console.log( "tree-sitter: tree.rootNode.children=", tree.rootNode.children) ;
+		
+		var child = tree.rootNode.child(0);
+
+		console.log( "tree-sitter: child.type=", child.type );
 
 
 	}
@@ -117,6 +179,24 @@
 
 	function treeSitterFileChange(file) {
 		console.log("tree-sitter:treeSitterFileChange: file.path=" + file.path + " ");
+
+		/*
+
+			// Replace 'let' with 'const'
+			const newSourceCode = 'const x = 1; console.log(x);';
+
+			tree.edit({
+			startIndex: 0,
+			oldEndIndex: 3,
+			newEndIndex: 5,
+			startPosition: {row: 0, column: 0},
+			oldEndPosition: {row: 0, column: 3},
+			newEndPosition: {row: 0, column: 5},
+			});
+
+			const newTree = parser.parse(newSourceCode, tree);
+		*/
+
 	}
 
 	function loadTreeSitter() {
