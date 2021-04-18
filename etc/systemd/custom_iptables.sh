63fb130bfbafb8f5b001a917ac29aa3193be3445
Z <zeta@zetafiles.org>
2021-04-18 10:06:53 +0200

    "mining pool communicates over UDP !?"

--- etc/systemd/custom_iptables.sh
+++ etc/systemd/custom_iptables.sh
@@ -161,8 +161,8 @@ iptables -A OUTPUT -p tcp --dport 25 -j REJECT
 # Block crypto miners
 # mine.c3pool.com
 # note: ip6tables wants ipv6 IP
-iptables -A OUTPUT -p tcp -d 18.130.178.39 -j REJECT
-iptables -A OUTPUT -p tcp -d 51.79.220.193 -j REJECT
+iptables -A OUTPUT -d 18.130.178.39 -j REJECT
+iptables -A OUTPUT -d 51.79.220.193 -j REJECT
 
 
 
