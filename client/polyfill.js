a92ccaeca32e8f116465e45a908080d68552450a
Z <zeta@zetafiles.org>
2022-02-23 21:30:04 +0100

    "Make new Event() work like other browsers in IE"

--- client/polyfill.js
+++ client/polyfill.js
@@ -234,7 +234,21 @@ defineProperty(String.prototype, 'codePointAt', {
 } else {
 String.prototype.codePointAt = codePointAt;
 }
-}());
+	}());
+}
+
+if(typeof CustomEvent == "undefined" || typeof Event == "object") { 
+	/*
+		Note: IE has Event, but you can't use new Event() or IE will throw an error! 
+		If CustomEvent exist, likely you can use new Event() as expected!?
+	*/
+	Event.prototype = window.Event.prototype;
+	Event = function CustomEvent ( event, params ) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		var evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	}
 }
 
 
