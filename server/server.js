df5b5f84af77c901d5b3ad76bdb65ae0e8216cfa
Z <zeta@zetafiles.org>
2022-10-08 19:01:49 +0200

    "open was sent to the terminal server!
    johan@webide:/home/johan/.webide/storage$ open state_new%20file
    /usr/bin/open: 811: : Permission denied "

--- server/server.js
+++ server/server.js
@@ -1411,6 +1411,15 @@ function main() {
 		});
 	}
 
+	if(info.uid == 0 && !USERNAME && HOSTNAME) {
+		// When typing open in the terminal emulator we always want the file to be open in the editor! And not give confusing errors like: /usr/bin/open: 811: : Permission denied
+		module_child_process.exec("unlink /usr/bin/open", EXEC_OPTIONS, function(err, stdout, stderr) {
+			if(err) console.error( err );
+			if(stderr) log(stderr, NOTICE);
+			if(stdout) log(stdout, INFO);
+		});
+	}
+
 	if(info.uid == 0 && !USERNAME && 1==2) {
 		// ## forkstat
 		// Detect when users start an app
