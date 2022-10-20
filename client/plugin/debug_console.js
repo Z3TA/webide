a507483e916a73a3a598364b86f63764cf4d8ccc
Z <zeta@zetafiles.org>
2022-10-20 15:12:30 +0200

    "colors"

--- client/plugin/debug_console.js
+++ client/plugin/debug_console.js
@@ -63,6 +63,7 @@
 
 		});
 
+		EDITOR.addPreRender(colorDebug);
 	}
 
 	function disableDebugConsole() {
@@ -70,6 +71,8 @@
 		console.log = consoleLogOriginal;
 		console.warn = consoleWarnOriginal;
 		console.error = consoleErrorOriginal;
+
+		EDITOR.removePreRender(colorDebug);
 	}
 
 	function captureConsoleLog(logType) {
@@ -81,9 +84,9 @@
 			}
 
 
-			var message = timeStamp() + " " + logType + ": " + str + " " + stackTrace();
+			var message = timeStamp() + " " + logType + " " + stackTrace() + "\n" + str + "";
 
-			consoleLogOriginal("captureConsoleLog: message.length=" + message.length + " " + UTIL.shortString(message));
+			//consoleLogOriginal("captureConsoleLog: message.length=" + message.length + " " + UTIL.shortString(message));
 
 			if(!FILE_WRITE_RECURSION) {
 				logFile.write(buffer + message, true);
@@ -137,9 +140,60 @@
 		stack.shift();
 		if(stack.lenth == 0) return "";
 
-		return "@" + stack[0].fun + " " + stack[0].source + ":" + stack[0].line;
+		var src = stack[0].source.replace(document.location.origin, "");
+
+		return "@" + stack[0].fun + " " + src + ":" + stack[0].line;
 	}
 
+
+	var reFirstLine = /^(\d+:\d+:\d+) (log|warn|error) (.*):(\d+)$/;
+	function colorDebug(buffer, file, bufferStartRow, maxColumns) {
+		
+		//consoleLogOriginal("colorDebug! bufferStartRow=" + bufferStartRow + " buffer.length=" + buffer.length);
+
+		var gray = UTIL.makeColorTransparent(EDITOR.settings.style.textColor, 50)
+
+		var text = "", match = null, time = "", logType = "", src="", lineNr = "", bufferRow = 0;
+		for(var row=bufferStartRow; row<(bufferStartRow+buffer.length); row++) {
+			
+			text = file.rowText(row);
+			match = text.match(reFirstLine);
+
+			//consoleLogOriginal("colorDebug: text: " + match);
+			//consoleLogOriginal("colorDebug: match: " + match);
+
+			if(!match) continue;
+
+			time = match[1];
+			logType = match[2];
+			src = match[3];
+			lineNr = match[4];
+
+			bufferRow = row-bufferStartRow;
+
+			color(buffer[bufferRow], 0, time.length, gray);
+			
+			if(logType == "log") color(buffer[bufferRow], time.length+1, logType.length, gray);
+			if(logType == "warn") color(buffer[bufferRow], time.length+1, logType.length, EDITOR.settings.style.colorYellow);
+			if(logType == "error") color(buffer[bufferRow], time.length+1, logType.length, EDITOR.settings.style.colorRed);
+
+			color(buffer[bufferRow], time.length+1 + logType.length+1, src.length, gray);
+
+			color(buffer[bufferRow], text.length-lineNr.length-1, 1, gray); // The colon before lineNr
+
+			color(buffer[bufferRow], text.length-lineNr.length, lineNr.length, gray);
+
+		}
+
+		return buffer;
+	}
+
+	function color(gridRow, start, len, color) {
+		for(var i=start; i<start+len; i++) {
+			//consoleLogOriginal("gridRow.length=" + gridRow.length + " start=" + start + " len=" + len + " gridRow[" + i + "]=" + gridRow[i]);
+			gridRow[i].color = color;
+		}
+	}
 
 })();
 
