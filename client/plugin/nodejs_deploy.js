da952901d5e36e3d624a4458ee115cbdf84331c3
Z <zeta@zetafiles.org>
2022-12-03 23:47:06 +0100

    "Working on the "deployed script" list ..."

--- client/plugin/nodejs_deploy.js
+++ client/plugin/nodejs_deploy.js
@@ -22,14 +22,20 @@
 			}
 			
 			function checkInitService() {
+				console.log("nodejs_deploy: Checking of node_init service is available...");
 				CLIENT.cmd("nodejs_init_ping", {}, function(err, resp) {
 					if(err) {
-						console.warn("loadNodeJsDeploy: Init serivice problem: " + err.message);
+						console.warn("nodejs_deploy: Init serivice problem: " + err.message);
+						
+						// Should we keep checking in case the service comes online !?
+						// or add a menu entry for connecting to prod service !?
+						if(EDITOR.settings.devMode) setTimeout(checkInitService, 5000);
+					
 						return;
 					}
 					
 					if(!resp.online) {
-						console.warn("loadNodeJsDeploy: Init serivice not online! " + JSON.stringify(resp));
+						console.warn("nodejs_deploy: Init serivice not online! " + JSON.stringify(resp));
 					}
 					else {
 						
@@ -96,29 +102,96 @@
 	function nodejsProdList() {
 		if(!deployedScriptWidget) deployedScriptWidget = EDITOR.createWidget(buildDeployedScriptManager);
 
-		deployedScriptWidget.show();
 		winMenuProdList.hide();
 
 		askForPassword(" in order to list scripts:", function (err, pw) {
 			if(err) return alertBox(err.message);
 			CLIENT.cmd("nodejs_init_list", {pw: pw}, function(err, resp) {
 				if(err) {
-					throw err;
+					return alertBox(err.message);
 				}
 
-				alertBox(JSON.stringify(resp));
+				console.log("nodejs_deploy: nodejsProdList: " + JSON.stringify(resp, null, 2));
+
+				if(!deployedScriptWidget.mainElement) deployedScriptWidget.show(); // Build the UI
+
+				while (scriptList.firstChild) scriptList.removeChild(scriptList.lastChild); // Empty list
+				
+				for(var script in resp.scripts) {
+					scriptList.appendChild( makeListItem(resp.scripts[script]) );
+				}
+
+				deployedScriptWidget.show(); // Will call EDITOR.resizeNeeded();
 
 			});
 		});
+	}
+
+	function makeListItem(script) {
+		console.log("nodejs_deploy:makeListItem: script=" + JSON.stringify(script));
+		// {"main":"/home/ltest1/.webide/prod/hello_world/hello.js","name":"hello_world","pathToFolder":"/home/ltest1/.webide/prod/hello_world/","log":"/home/ltest1/log/hello_world.log","running":true}
+
+		var stats = "stopped";
+		if(script.running) status = "enabled";
+
+		var tr = document.createElement("tr");
+
+		var name = document.createElement("td");
+		name.innerText = script.name;
+
+
+		var mainScript = document.createElement("a");
+		mainScript.innerText = script.main;
+		mainScript.onclick = function openFile() {
+			EDITOR.openFile(script.main);
+		}
+
+		var main = document.createElement("td");
+		main.appendChild(mainScript);
+
+		var status = document.createElement("td");
+		status.innerText = "Entry point";
+
+		tr.appendChild(name);
+		tr.appendChild(main);
+		tr.appendChild(status);
+
+		return tr;
 	}
 
 	function buildDeployedScriptManager() {
 
 		var main = document.createElement("div");
 
-		scriptList = document.createElement("ul");
+		var table = document.createElement("table");
+		table.classList.add("data");
 
-		main.appendChild(scriptList);
+		var thead = document.createElement("thead");
+		var tbody = document.createElement("tbody");
+
+		var tr = document.createElement("tr");
+
+		var name = document.createElement("th");
+		name.innerText = "Name";
+
+		var mainScript = document.createElement("th");
+		mainScript.innerText = "Entry point";
+
+		var status = document.createElement("th");
+		status.innerText = "Entry point";
+
+		tr.appendChild(name);
+		tr.appendChild(mainScript);
+		tr.appendChild(status);
+
+		thead.appendChild(tr);
+		
+		table.appendChild(thead);
+		table.appendChild(tbody);
+
+		scriptList = tbody;
+
+		main.appendChild(table);
 
 		return main;
 	}
@@ -229,7 +302,7 @@
 		function readPj(folder) {
 			if(folder == undefined) throw new Error("folder=" + folder);
 			
-			//console.log("Looking for package.json in folder=" + folder + " ..."); 
+			//console.log("nodejs_deploy: Looking for package.json in folder=" + folder + " ..."); 
 		EDITOR.readFromDisk(folder + "package.json", function fileRead(readFileErr, filePath, fileContent) {
 				if(folder == undefined) throw new Error("folder=" + folder);
 				
